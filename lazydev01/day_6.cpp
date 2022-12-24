27d07d1d047c0434976cba6ea4c330b752e8e660
lazydev01 <arpeetbhavsar01@gmail.com>
2022-12-24 21:06:16 +0530

    day_6

---
+++ lazydev01/day_6.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef ONLINE_JUDGE
+  #define cerr if(0) cerr
+#endif
+
+#define int long long int
+#define endl "\n"
+#define YES cout<<"YES\n"
+#define NO cout<<"NO\n"
+
+int32_t main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+
+        int n;
+        cin >> n;
+        vector<int> a(n);
+        vector<int> b(n);
+        for(int i=0; i<n; i++){
+            cin >> a[i];
+        }
+        for(int i=0; i<n; i++){
+            cin >> b[i];
+        }
+        for(int i=0; i<n; i++){
+            a[i]=(a[i]*20)-(b[i]*10);
+            if(a[i]<0){
+                a[i]=0;
+            }
+        }
+        cout << *max_element(a.begin(), a.end()) << endl;
+    }
+    return 0;
+}
