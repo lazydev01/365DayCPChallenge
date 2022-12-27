c85c78f1c9e8d6e4e4a3dc7d34d24d6cb9c4a46e
lazydev01 <arpeetbhavsar01@gmail.com>
2022-12-27 18:30:03 +0530

    day_9

---
+++ lazydev01/day_9.cpp
@@ -0,0 +1,19 @@
+/*
+
+Question -> https://www.codechef.com/problems/CHFTIRED
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	while(t--){
+	    int a, b;
+	    cin >> a >> b;
+	    cout << "YES" << endl;
+	}
+	return 0;
+}
