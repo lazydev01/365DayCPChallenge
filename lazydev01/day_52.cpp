9ae61f330a6e84f295b9ca092a78c72307536108
lazydev01 <arpeetbhavsar01@gmail.com>
2023-02-08 20:42:31 +0530

    day_52

---
+++ lazydev01/day_52.cpp
@@ -0,0 +1,38 @@
+/*
+
+    Question -> https://www.codechef.com/problems/TAXES
+
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef ONLINE_JUDGE
+  #define cerr if(0) cerr
+#endif
+
+#define int long long int
+#define endl "\n"
+#define YES cout<<"YES\n"
+#define NO cout<<"NO\n"
+
+int32_t main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+
+        int x;
+        cin >> x;
+        if(x>100){
+            x-=10;
+        }
+        cout << x << endl;
+
+    }
+    return 0;
+}
