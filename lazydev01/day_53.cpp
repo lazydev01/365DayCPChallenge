abd85cdb79e86bdd475dde17da686c9d9428a523
lazydev01 <98304349+lazydev01@users.noreply.github.com>
2023-02-09 19:30:04 +0530

    Create day_53.cpp

---
+++ lazydev01/day_53.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int test_case;
+    cin >> test_case;
+    while(test_case--){
+        int x;
+        cin >> x;
+        if(x>=67 && x<=45000){
+            cout << "YES" << endl;
+        }
+        else{
+            cout << "NO" << endl;
+        }
+    }
+	return 0;
+}
