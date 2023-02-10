6ff1b6c14610c3a3281beb06d6be5e3b2b6daaea
lazydev01 <arpeetbhavsar01@gmail.com>
2023-02-10 20:31:35 +0530

    day_53

---
+++ lazydev01/day_53.cpp
@@ -0,0 +1,40 @@
+/*
+
+    Question -> https://www.codechef.com/problems/TIMELY
+
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef ONLINE_JUDGE
+  #define cerr if(0) cerr
+#endif
+
+#define int long long int
+#define endl "\n"
+#define YES cout<<"YES\n"
+#define NO cout<<"NO\n"
+
+int32_t main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+
+        int x;
+        cin >> x;
+        if(x>=30){
+            YES;
+        }
+        else{
+            NO;
+        }
+
+    }
+    return 0;
+}
