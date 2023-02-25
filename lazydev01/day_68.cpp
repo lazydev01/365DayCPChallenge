506d065c939e4cfcfa9785923ac40409dc68e105
lazydev01 <arpeetbhavsar01@gmail.com>
2023-02-25 22:27:58 +0530

    day_68

---
+++ lazydev01/day_68.cpp
@@ -0,0 +1,40 @@
+/*
+
+    Question -> 
+
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef ONLINE_JUDGE
+  #define cerr if(0) cerr
+#endif
+
+#define int long long int
+#define endl "\n"
+#define YES cout<<"YES\n"
+#define NO cout<<"NO\n"
+
+int32_t main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+
+        int n;
+        cin >> n;
+        if(n%2==0){
+            cout << "YES" << endl;
+        }
+        else{
+            cout << "NO" << endl;
+        }
+
+    }
+    return 0;
+}
