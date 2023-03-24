3967b0bd34724307dff8625670e595713b5c70d9
lazydev01 <arpeetbhavsar01@gmail.com>
2023-03-24 21:16:29 +0530

    day_95

---
+++ lazydev01/day_95.cpp
@@ -0,0 +1,47 @@
+-/*
+
+    Question -> https://www.codechef.com/problems/MIXCOLOR
+
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef ONLINE_JUDGE
+  #define cerr if(0) cerr
+#endif
+
+#define int long long int
+#define endl "\n"
+#define YES cout<<"YES\n"
+#define NO cout<<"NO\n"
+
+int32_t main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+
+        int n;
+        cin >> n;
+        vector<int> arr(n);
+        for(int i=0; i<n; i++){
+            cin >> arr[i];
+        }
+        map<int, int> m;
+        for(int i=0; i<n; i++){
+            m[arr[i]]++;
+        }
+        int sum = 0;
+        for(auto i : m){
+            sum+=(i.second-1);
+        }
+        cout << sum << endl;
+
+    }
+    return 0;
+}
