7a4aaeb172cf3a09395ba2f68d4eb192c300c6e5
lazydev01 <arpeetbhavsar01@gmail.com>
2023-04-20 22:29:52 +0530

    day_122]

---
+++ lazydev01/day_122.cpp
@@ -0,0 +1,29 @@
+/*
+
+    Question -> https://www.codechef.com/problems/CCLEARN
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef ONLINE_JUDGE
+  #define cerr if(0) cerr
+#endif
+
+#define int long long int
+#define endl "\n"
+#define YES cout<<"YES\n"
+#define NO cout<<"NO\n"
+
+int32_t main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+
+
+        int n;
+        cin >> n;
+        cout << 2*n << endl;
+    return 0;
+}
