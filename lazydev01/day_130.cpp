421d1249f87fa50f6f9d33193fb3dd6ba141f550
lazydev01 <arpeetbhavsar01@gmail.com>
2023-04-28 06:42:48 +0530

    day_130

--- lazydev01/day_130.cpp
+++ lazydev01/day_130.cpp
@@ -25,3 +25,19 @@
         }
         return ans;
     }
+
+    int pivotIndex(vector<int>& nums) {
+        int sum = 0;
+        for(int i=0; i<nums.size(); i++){
+            sum+=nums[i];
+        }
+        int left_sum = 0;
+        for(int i=0; i<nums.size(); i++){
+            sum-=nums[i];
+            if(left_sum==sum){
+                return i;
+            }
+            left_sum+=nums[i];
+        }
+        return -1;
+    }
