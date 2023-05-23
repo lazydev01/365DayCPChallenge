d9a62d2e45eca7af311028c5811aee4104129340
lazydev01 <arpeetbhavsar01@gmail.com>
2023-05-23 07:00:20 +0530

    day_155

--- lazydev01/day_155.cpp
+++ lazydev01/day_155.cpp
@@ -13,11 +13,31 @@ using namespace std;
 void longestSubarrayWithSumKOnlyPositives(){
     int n, k;
     cin >> n >> k;
-    int start=0;
-    int end=0;
-    while(end<n){
-        
+    vector<int> arr(n);
+    for(int i=0; i<n; i++){
+      cin >> arr[i];
     }
+    int start = 0;
+    int end = 0;
+    int count = 0;
+    int sum = 0;
+    while(end<n){
+      if(sum==k){
+        count = max(count, end-start+1);
+        end++;
+        sum+=arr[end];
+      }
+      else if(sum>k){
+        sum -= arr[start];
+        start++;
+      }
+      else{
+        sum+=arr[end];
+        end++;
+      }
+      cout << sum << " " << start << " " << end << endl;
+    }
+    cout << count << endl;
 }
 
 int32_t main()
@@ -25,6 +45,6 @@ int32_t main()
 ios_base::sync_with_stdio(false);
 cin.tie(NULL);
 
-    
+    longestSubarrayWithSumKOnlyPositives();
     return 0;
 }
