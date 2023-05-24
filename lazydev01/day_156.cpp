0b9c7162701a2a5c40e30ecfbdbe5370aa6dfad6
lazydev01 <arpeetbhavsar01@gmail.com>
2023-05-24 22:06:37 +0530

    day_156

--- lazydev01/day_156.cpp
+++ lazydev01/day_156.cpp
@@ -44,11 +44,71 @@ void sumOfTwoNumbersEqualToTarget(){
     }
 }
 
+void sumOfTwoNumbersEqualToTargetReturnIndex(){
+    int n, target;
+    cin >> n >> target;
+    vector<int> arr(n);
+    for(int i=0; i<n; i++){
+        cin >> arr[i];
+    }
+    map<int, int> m;
+    vector<int> ans;
+    for(int i=0; i<n; i++){
+        if(m.find(target-arr[i])!=m.end()){
+            ans.push_back(m.find(target-arr[i])->second);
+            ans.push_back(i);
+            break;
+        }
+        else{
+            m[arr[i]] = i;
+        }
+    }
+    if(ans.size() > 0){
+        cout << ans[0] << " " << ans[1] << endl; 
+    }
+    else{
+        cout << 0 << " " << 0 << endl;
+    }
+}
+
+void sumOfTwoNumbersEqualToTargetOptimal(){
+    int n, target;
+    cin >> n >> target;
+    vector<int> arr(n);
+    for(int i=0; i<n; i++){
+        cin >> arr[i];
+    }
+    sort(arr.begin(), arr.end());
+    int start = 0;
+    int end = n-1;
+    bool flag = false;
+    while(start<end){
+        if((arr[start] + arr[end]) == target){
+            flag = true;
+            break;
+        }
+        else if((arr[start] + arr[end]) > target){
+            end--;
+        }
+        else{
+            start++;
+        }
+    }
+    if(flag){
+        cout << "YES" << endl;
+    }
+    else{
+        cout << "NO" << endl;
+    }
+}
+
 int32_t main()
 {
 ios_base::sync_with_stdio(false);
 cin.tie(NULL);
 
-    sumOfTwoNumbersEqualToTarget();
+    // sumOfTwoNumbersEqualToTarget();
+    // sumOfTwoNumbersEqualToTargetReturnIndex();
+    sumOfTwoNumbersEqualToTargetOptimal();
     return 0;
 }
