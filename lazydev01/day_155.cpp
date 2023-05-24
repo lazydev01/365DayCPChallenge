2c51fc1aea19a69caa1ce9a1aae1723888d4f259
lazydev01 <arpeetbhavsar01@gmail.com>
2023-05-24 06:53:54 +0530

    day_155

--- lazydev01/day_155.cpp
+++ lazydev01/day_155.cpp
@@ -18,26 +18,48 @@ void longestSubarrayWithSumKOnlyPositives(){
       cin >> arr[i];
     }
     int start = 0;
-    int end = 0;
+    long sum = 0;
     int count = 0;
-    int sum = 0;
-    while(end<n){
-      if(sum==k){
-        count = max(count, end-start+1);
-        end++;
-        sum+=arr[end];
-      }
-      else if(sum>k){
-        sum -= arr[start];
+    int maxCount = 0;
+    for(int i=0; i<n; i++){
+      sum+=arr[i];
+      while(sum > k){
+        sum-=arr[start];
         start++;
       }
-      else{
-        sum+=arr[end];
-        end++;
+      if(sum==k){
+        count = i-start+1;
+        maxCount = max(maxCount,count);
       }
-      cout << sum << " " << start << " " << end << endl;
     }
-    cout << count << endl;
+    cout << maxCount << endl;
+}
+
+void longestSubarrayWithSumK(){
+  int n, k;
+  cin >> n >> k;
+  vector<int> arr(n);
+  for(int i=0; i<n; i++){
+    cin >> arr[i];
+  }
+  long sum = 0;
+  int maxCount = 0;
+  map<long long, int> m;
+  for(int i=0; i<n; i++){
+    sum+=arr[i];
+    if(sum==k){
+      maxCount = max(maxCount, i+1);
+    }
+    long remaining = sum - arr[i];
+    if(m.find(remaining)!=m.end()){
+      int distance = i - m[remaining];
+      maxCount = max(maxCount, distance);
+    }
+    if(m.find(remaining)==m.end()){
+      m[sum] = i;
+    }
+  }
+  cout << maxCount << endl;
 }
 
 int32_t main()
@@ -45,6 +67,7 @@ int32_t main()
 ios_base::sync_with_stdio(false);
 cin.tie(NULL);
 
-    longestSubarrayWithSumKOnlyPositives();
+    // longestSubarrayWithSumKOnlyPositives();
+    longestSubarrayWithSumK();
     return 0;
 }
