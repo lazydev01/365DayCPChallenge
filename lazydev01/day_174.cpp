ece4422182abdc6ea38afbadcd54ea994acd7d09
lazydev01 <arpeetbhavsar01@gmail.com>
2023-06-11 22:55:49 +0530

    day_174

--- lazydev01/day_174.cpp
+++ lazydev01/day_174.cpp
@@ -48,6 +48,37 @@ void fourSumOptimal(){
         cin >> arr[i];
     }
     sort(arr.begin(), arr.end());
+    vector<vector<int>> ans;
+    for(int i=0; i<n; i++){
+        if(i>0 && arr[i]==arr[i-1])continue;
+        for(int j=i+1; j<n; j++){
+            if(j>(i+1) && arr[j]==arr[j-1])continue;
+            int k = j + 1;
+            int l = n - 1;
+            while(k<l){
+                int sum = arr[i] + arr[j];
+                sum+=arr[k];
+                sum+=arr[l];
+                if(sum==target){
+                    cout << "HI" << endl;
+                    vector<int> temp = {arr[i], arr[j], arr[k], arr[l]};
+                    ans.push_back(temp);
+                    k++;
+                    l--;
+                    while(k<l && arr[k]==arr[k-1])k++;
+                    while(k<l && arr[l]==arr[l+1])l--;
+                }
+                else if(sum>target)l--;
+                else k++;
+            }
+        }
+    }
+    for(int i=0; i<ans.size(); i++){
+        for(int j=0; j<4; j++){
+            cout << ans[i][j] << " ";
+        }
+        cout << endl;
+    }
 
 }
 
@@ -56,6 +87,7 @@ int32_t main()
 ios_base::sync_with_stdio(false);
 cin.tie(NULL);
 
-    fourSum();
+    // fourSum();
+    fourSumOptimal();
     return 0;
 }
