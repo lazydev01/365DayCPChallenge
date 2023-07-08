e84f46d41ba9bebaecd77494471e5b95271dda82
lazydev01 <arpeetbhavsar01@gmail.com>
2023-07-08 20:09:20 +0530

    day_201

---
+++ lazydev01/day_201.cpp
@@ -0,0 +1,27 @@
+/*
+
+    Question -> https://www.codechef.com/problems/RIP2000
+
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef ONLINE_JUDGE
+  #define cerr if(0) cerr
+#endif
+
+#define int long long int
+#define endl "\n"
+#define YES cout<<"YES\n"
+#define NO cout<<"NO\n"
+
+int32_t main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+
+    int n;
+    cin >> n;
+    cout << 4*n << endl;
+    return 0;
+}
