d255bfe31a684d0ac60c98fe3516d8aae8a86a9b
lazydev01 <arpeetbhavsar01@gmail.com>
2023-07-14 20:53:13 +0530

    day_207

---
+++ lazydev01/day_207.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef ONLINE_JUDGE
+  #define cerr if(0) cerr
+#endif
+
+#define int long long int
+#define endl "\n"
+#define YES cout<<"YES\n"
+#define NO cout<<"NO\n"
+
+void combinationSum2(){
+    int n, target;
+    cin >> n >> target;
+    vector<int> arr(n);
+    for(int i=0; i<n; i++){
+        cin >> arr[i];
+    }
+    sort(arr.begin(), arr.end());
+}
+
+/*
+
+    Question -> https://www.codechef.com/problems/MVR
+
+*/
+void messiVsRonaldo(){
+    int a, b, x, y;
+    cin >> a >> b >> x >>y;
+    if((2*a + b)>(2*x + y)){
+        cout << "Messi" << endl;
+    }
+    else if((2*a + b)<(2*x + y)){
+        cout << "Ronaldo" << endl;
+    }
+    else{
+        cout << "Equal" << endl;
+    }
+}
+
+int32_t main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+
+    
+    return 0;
+}
