cb542e1dadcc1fc397ab5c8b47bc1530c51c375e
lazydev01 <arpeetbhavsar01@gmail.com>
2023-07-15 19:45:35 +0530

    day_208

---
+++ lazydev01/day_208.cpp
@@ -0,0 +1,41 @@
+/*
+
+    Question -> https://www.codechef.com/problems/FAIRPASS
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef ONLINE_JUDGE
+  #define cerr if(0) cerr
+#endif
+
+#define int long long int
+#define endl "\n"
+#define YES cout<<"YES\n"
+#define NO cout<<"NO\n"
+
+int32_t main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+
+        int n, k;
+        cin >> n >> k;
+        if(k>=(n+1)){
+            cout << "YES" << endl;
+        }
+        else{
+            cout << "NO" << endl;
+        }
+
+    }
+    return 0;
+}
