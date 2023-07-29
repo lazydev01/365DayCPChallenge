4dc885e2feb38756e9a914824e659de3024fbcdd
lazydev01 <arpeetbhavsar01@gmail.com>
2023-07-29 19:53:38 +0530

    day_222

---
+++ lazydev01/day_222.cpp
@@ -0,0 +1,28 @@
+/*
+
+    Question -> https://www.codechef.com/problems/COUGAME
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef ONLINE_JUDGE
+  #define cerr if(0) cerr
+#endif
+
+#define int long long int
+#define endl "\n"
+#define YES cout<<"YES\n"
+#define NO cout<<"NO\n"
+
+int32_t main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+
+    int g, b;
+    cin >> g >> b;
+    cout << b-g << endl;
+    return 0;
+}
