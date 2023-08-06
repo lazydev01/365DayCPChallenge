ec8004ef3806cfba5982047fbcd3b9f2281c8f7a
lazydev01 <arpeetbhavsar01@gmail.com>
2023-08-06 19:30:12 +0530

    Minor changes to House Robber

--- lazydev01/day_230.cpp
+++ lazydev01/day_230.cpp
@@ -147,6 +147,11 @@ long long int houseRobberSpaceOptimization(vector<int>& valueInHouse)
     return max(firstIgnored, lastIgnored);
 }
 
+int ninjaTraining(int n, vector<vector<int>> &points)
+{
+    
+}
+
 int32_t main()
 {
 ios_base::sync_with_stdio(false);
