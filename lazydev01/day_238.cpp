993f069a0a75a805358ac81fba33be58b8b88653
lazydev01 <arpeetbhavsar01@gmail.com>
2023-08-15 21:27:14 +0530

    Codeforces Round 893 (Div. 2) Problem A Solved, Problem B Unsolved

--- lazydev01/day_238.cpp
+++ lazydev01/day_238.cpp
@@ -41,11 +41,66 @@ double median(vector<int>& a, vector<int>& b) {
     }
 }
 
+// Codeforces Round 893 (Div. 2)
+
+void problemA(){
+    int t;
+    cin >> t;
+    while(t--){
+        int a, b, c;
+        cin >> a >> b >> c;
+        if(c%2==0){
+            if(a>b){
+                cout << "First" << endl;
+            }
+            else{
+                cout << "Second" << endl;
+            }
+        }
+        else{
+            if(a>=b){
+                cout << "First" << endl;
+            }
+            else{
+                cout << "Second" << endl;
+            }
+        }
+    }
+}
+
+void problemB(){
+    int t;
+    cin >> t;
+    while(t--){
+        int n, m, d;
+        cin >> n >> m >> d;
+        vector<int> cookieSeller(m);
+        for(int i=0; i<m; i++){
+            cin >> cookieSeller[i];
+        }
+        if(cookieSeller[0]!=1){
+            cookieSeller.insert(cookieSeller.begin(), 1);
+        }
+        cookieSeller.push_back(n);
+        int noOfCookies = 1;
+        int noOfCookieSellers = 0;
+        for(int i=1; i<cookieSeller.size()-1; i++){
+            noOfCookies += ((cookieSeller[i]-cookieSeller[i-1]-1)/d)+1;
+            if(((cookieSeller[i+1]-cookieSeller[i-1])/d)!=(((cookieSeller[i]-cookieSeller[i-1])/d)+1)){
+                noOfCookieSellers++;
+            } 
+        }
+        noOfCookies += (n-cookieSeller[cookieSeller.size()-2])/d;
+        cout << noOfCookies-1 << " " << noOfCookieSellers << endl;
+    }
+}
+
 int32_t main()
 {
 ios_base::sync_with_stdio(false);
 cin.tie(NULL);
 
-    
+    // problemA();
+    problemB();
     return 0;
 }
