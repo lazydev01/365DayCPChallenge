9acc9c6cd9a825cda6a0fef60864c8a421aa556e
lazydev01 <arpeetbhavsar01@gmail.com>
2023-08-25 22:08:59 +0530

    Count Partition Recursion and Memoization

--- lazydev01/day_248.cpp
+++ lazydev01/day_248.cpp
@@ -36,8 +36,64 @@ int findWaysSpaceOptimization(vector<int>& arr, int k)
     return prev[k];
 }
 
+int mod = 1e9 + 7;
+
+int countWays(int ind, int target, vector<int> &arr){
+    if(ind==0){
+        if(target==0 && arr[0]==0) return 2;
+        if(target==0 || arr[0]==target) return 1;
+        return 0;
+    }
+    int notTake = countWays(ind-1, target, arr);
+    int take = 0;
+    if(arr[ind]<=target){
+        take = countWays(ind-1, target-arr[ind], arr);
+    }
+    return (take + notTake)%mod;
+}
+
 int countPartitions(int n, int d, vector<int> &arr) {
-    
+    int totSum=0;
+    for(auto &ele : arr) totSum += ele;
+    if(totSum-d< 0 || (totSum-d)%2==1) return 0;
+    return countPartitions(n-1,(totSum-d)/2 , arr);
+}
+
+int countWaysMemoization(int ind, int target, vector<int> &arr, vector<vector<int>> &dp){
+    if(ind==0){
+        if(arr[0]==0 &&  target==0) return 2;
+        if(arr[0]==0 || target==arr[0]) return 1;
+        return 0;
+    }
+    if(dp[ind][target]!=-1){
+        return dp[ind][target];
+    }
+    int notTake = countWaysMemoization(ind-1, target, arr, dp);
+    int take = 0;
+    if(arr[ind]<=target){
+        take = countWaysMemoization(ind-1, target-arr[ind], arr, dp);
+    }
+    return (notTake + take)%mod;
+}
+
+int countPartitionsMemoization(int n, int d, vector<int> &arr) {
+    int totSum = 0;
+    for(int i=0; i<arr.size(); i++) totSum += arr[i];
+    if(totSum-d < 0 || (totSum-d)%2==1) return 0;
+    vector<vector<int>> dp(n, vector<int>(((totSum-d)/2)+1, -1));
+    return countWaysMemoization(n-1, (totSum - d)/2, arr, dp);
+}
+
+int countPairsTabulation(int n, int d, vector<int> arr){
+    int totSum = 0;
+    for(int i=0; i<n; i++){
+        totSum += arr[i];
+    }
+    if(totSum-d<0 || (totSum-d)%2==1){
+        return 0;
+    }
+    int target = (totSum - d)/2;
+    vector<vector<int>> dp(n, vector<int>(target + 1, 0));
 }
 
 int32_t main()
