c7ce4d7403b485659328f1df2bb252afcf25fd90
lazydev01 <arpeetbhavsar01@gmail.com>
2023-08-26 12:34:07 +0530

    Knapsack Recursion, Memoization and Tabulation

--- lazydev01/day_249.cpp
+++ lazydev01/day_249.cpp
@@ -54,6 +54,25 @@ int knapsackMemoization(vector<int> weight, vector<int> value, int n, int maxWei
     return fetchMaxValMemoization(n-1, maxWeight, weight, value, dp);
 }
 
+int knapsackTabulation(vector<int> weight, vector<int> value, int n, int maxWeight) 
+{
+    vector<vector<int>> dp(n, vector<int>(maxWeight + 1, 0));
+    for(int i=weight[0]; i<=maxWeight; i++){
+        dp[0][i] = value[0];
+    }
+    for(int i=1; i<n; i++){
+        for(int j=0; j<=maxWeight; j++){
+            int notTake = 0 + dp[i-1][j];
+            int take = INT_MIN;
+            if(j >= weight[i]){
+                take = value[i] + dp[i-1][j-weight[i]];
+            }
+            dp[i][j] = max(take, notTake);
+        }
+    }
+    return dp[n-1][maxWeight];
+}
+
 int32_t main()
 {
 ios_base::sync_with_stdio(false);
