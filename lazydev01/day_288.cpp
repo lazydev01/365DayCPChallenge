e053c052c35209fc55f6421a662997e53d870843
lazydev01 <arpeetbhavsar01@gmail.com>
2023-10-04 08:07:00 +0530

    Codeforces Round 900 Problem C

--- lazydev01/day_288.cpp
+++ lazydev01/day_288.cpp
@@ -5,7 +5,7 @@ using namespace std;
   #define cerr if(0) cerr
 #endif
 
-#define int long long int
+// #define int long long int
 #define endl "\n"
 #define YES cout<<"YES\n"
 #define NO cout<<"NO\n"
@@ -16,20 +16,41 @@ using namespace std;
 
 */
 
+void problemC(){
+    int t;
+    cin >> t;
+    while(t--){
+        long long n, k, x;
+        cin >> n >> k >> x;
+        long long left = n-k;
+        long long sum_n = (n*(n+1))/2;
+        long long sum_left = (left * (left + 1))/2;
+        long long sum_k = (k*(k+1))/2;
+        long long max_sum = sum_n - sum_left;
+        if(x>max_sum || x<sum_k){
+            cout << "NO" << endl;
+        }
+        else{
+            cout << "YES" << endl;
+        }
+    }
+}
+
 int32_t main()
 {
 ios_base::sync_with_stdio(false);
 cin.tie(NULL);
 
-    int t;
-    cin >> t;
-    while(t--){
-        int n;
-        cin >> n;
-        for(int i=0; i<n; i++){
-            cout << (2*i)+1 << " ";
-        }
-        cout << endl;
-    }
-    return 0;
+    problemC();
+    // int t;
+    // cin >> t;
+    // while(t--){
+    //     int n;
+    //     cin >> n;
+    //     for(int i=0; i<n; i++){
+    //         cout << (2*i)+1 << " ";
+    //     }
+    //     cout << endl;
+    // }
+    // return 0;
 }
