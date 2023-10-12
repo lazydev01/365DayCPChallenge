7c9012d9a1bd1f274714c1208f2240110e10f64c
lazydev01 <arpeetbhavsar01@gmail.com>
2023-10-12 21:00:50 +0530

    Codechef Off By One

---
+++ lazydev01/day_296.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef ONLINE_JUDGE
+  #define cerr if(0) cerr
+#endif
+
+#define int long long int
+#define endl "\n"
+#define YES cout<<"YES\n"
+#define NO cout<<"NO\n"
+
+void fearOfTheDark(){
+    int t;
+    cin >> t;
+    while(t--){
+        int p1, p2;
+        cin >> p1 >> p1;
+        int a1, a2, b1, b2;
+        cin >> a1 >> a2 >> b1 >> b2;
+    }
+}
+
+void offByOne(){
+    int a, b;
+    cin >> a >> b;
+    cout << ((a+b)*10 + 1) << endl;
+}
+
+int32_t main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+
+    
+    return 0;
+}
