63a24d45ff25ed5687af839eddea8c3bcbc7fc74
lazydev01 <arpeetbhavsar01@gmail.com>
2023-11-08 06:38:02 +0530

    Codechef Lunchtime

---
+++ lazydev01/day_323.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef ONLINE_JUDGE
+  #define cerr if(0) cerr
+#endif
+
+#define int long long int
+#define endl "\n"
+#define YES cout<<"YES\n"
+#define NO cout<<"NO\n"
+
+void lunchtime(){
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        if(n>=1 && n<=4){
+            cout << "YES" << endl;
+        }
+        else{
+            cout << "NO" << endl;
+        }
+    }
+}
+
+int32_t main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+
+    lunchtime();
+    return 0;
+}
