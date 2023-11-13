134f62c23ccd836c46351e4970f5bbaaa633e937
lazydev01 <arpeetbhavsar01@gmail.com>
2023-11-13 13:05:10 +0530

    Codechef Starters 106 Save People

--- lazydev01/day_328.cpp
+++ lazydev01/day_328.cpp
@@ -36,11 +36,44 @@ void orPermutation(){
     }
 }
 
+void savePeople(){
+    int t;
+    cin >> t;
+    while(t--){
+        int m, n, x, y;
+        cin >> n >> m >> x >> y;
+        int top_area = 0, left_area = 0, right_area = 0, bottom_area = 0;
+        top_area = (x-1)*(m);
+        left_area = (y-1)*(n);
+        if(x!=n) bottom_area = (n-x)*(m);
+        if(y!=m) right_area = (m-y)*(n);
+        cout << max(top_area, max(left_area, max(bottom_area, right_area))) << endl;
+    }
+}
+
+
 int32_t main()
 {
 ios_base::sync_with_stdio(false);
 cin.tie(NULL);
 
-    orPermutation();
+    // orPermutation();
+    savePeople();
     return 0;
 }
+
+/*
+
+7 7
+6 5
+
+0 0 0 0 0 0 0
+0 0 0 0 0 0 0
+0 0 0 0 0 0 0
+0 0 0 0 0 0 0
+0 0 0 0 0 0 0
+0 0 0 0 1 0 0
+0 0 0 0 0 0 0
+
+
+*/
