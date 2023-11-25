a1567c17ff64730e8e1ad469f02b74177bbafe80
lazydev01 <arpeetbhavsar01@gmail.com>
2023-11-25 10:45:00 +0530

    Codechef Starters 109 Worth of a Video

---
+++ lazydev01/day_340.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef ONLINE_JUDGE
+  #define cerr if(0) cerr
+#endif
+
+#define int long long int
+#define endl "\n"
+#define YES cout<<"YES\n"
+#define NO cout<<"NO\n"
+
+void worthOfAVideo(){
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        cout << 24000 * n << endl;
+    }
+
+}
+
+int32_t main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+
+    worthOfAVideo();
+    return 0;
+}
