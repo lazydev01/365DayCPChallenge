e45d37c259c56a26fa296a11fc05eff257bc18b8
lazydev01 <arpeetbhavsar01@gmail.com>
2023-11-25 16:10:34 +0530

    Codechef Starters 109 Minimum XOR

--- lazydev01/day_340.cpp
+++ lazydev01/day_340.cpp
@@ -31,12 +31,34 @@ void bestOfNSets(){
     }
 }
 
+void minimumXOR(){
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        vector<int> arr(n);
+        int acc_xor = 0;
+        for(int i=0; i<n; i++){
+            cin >> arr[i];
+            acc_xor ^= arr[i];
+        }
+        int min_xor = acc_xor;
+        for(int i=0; i<n; i++){
+            int temp = acc_xor ^ arr[i];
+            min_xor = min(min_xor, temp);
+        }
+        cout << min_xor << endl;
+    }
+}
+
 int32_t main()
 {
 ios_base::sync_with_stdio(false);
 cin.tie(NULL);
 
     // worthOfAVideo();
-    bestOfNSets();
+    // bestOfNSets();
+    minimumXOR();
     return 0;
 }
