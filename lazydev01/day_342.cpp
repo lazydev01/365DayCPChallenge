7bea195712223ca5cbab67abc3caf44f338ae6eb
lazydev01 <arpeetbhavsar01@gmail.com>
2023-11-27 07:11:45 +0530

    Codechef Starters 108 Clan Expansion

--- lazydev01/day_342.cpp
+++ lazydev01/day_342.cpp
@@ -21,11 +21,48 @@ void aliceAndMarks(){
     }
 }
 
+void clanExpansion(){
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        vector<int> arr(n);
+        for(int i=0; i<n; i++){
+            cin >> arr[i];
+        }
+        map<int, vector<int>> mp;
+        for(int i=0; i<n; i++){
+            mp[arr[i]].push_back(i+1);
+        }
+        map<int, int> mp_ans;
+        for(auto i : mp){
+            int mini = 0;
+            for(int j=0; j<i.second.size()-1; j++){
+                mini = max(mini, (i.second[j+1] - i.second[j])/2);
+            }
+            mini = max(mini, i.second[0] - 1);
+            mini = max(mini, n - i.second[i.second.size() - 1]);
+            mp_ans[i.first] = mini;
+        }
+        int mini_clan = INT_MAX;
+        int ans = 0;
+        for(auto i : mp_ans){
+            if(mini_clan > i.second){
+                mini_clan = i.second;
+                ans = i.first;
+            }
+        }
+        cout << ans << " " << mini_clan << endl;
+    }
+}
+
 int32_t main()
 {
 ios_base::sync_with_stdio(false);
 cin.tie(NULL);
 
-    aliceAndMarks();
+    // aliceAndMarks();
+    clanExpansion();
     return 0;
 }
