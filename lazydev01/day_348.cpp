28cb5dbf5957a998069943914624c6795fb8bed1
lazydev01 <arpeetbhavsar01@gmail.com>
2023-12-03 15:25:00 +0530

    Codechef Starters 110 Monster

--- lazydev01/day_348.cpp
+++ lazydev01/day_348.cpp
@@ -39,11 +39,55 @@ void marbles(){
     }
 }
 
+bool isPrime(int n){
+    if(n<=1){
+        return false;
+    }
+    if(n==2){
+        return true;
+    }
+    for(int i=2; i*i<=n; i++){
+        if(n%i==0){
+            return false;
+        }
+    }
+    return true;
+}
+
+void monster(){
+    int t;
+    cin >> t;
+    while(t--){
+        int h;
+        cin >> h;
+        int i = 1;
+        int cnt = 0;
+        while(true){
+            if(isPrime(h)){
+                cout << cnt + 1 << endl;
+                break;
+            }
+            else if(h==0){
+                cout << cnt << endl;
+                break;
+            }
+            else if(h<0){
+                cout << -1 << endl;
+                break;
+            }
+            h-=i;
+            i*=2;
+            cnt++;
+        }
+    }
+}
+
 int32_t main()
 {
 ios_base::sync_with_stdio(false);
 cin.tie(NULL);
 
-    marbles();
+    // marbles();
+    monster();
     return 0;
 }
