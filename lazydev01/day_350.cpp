f74f8ec0b61daae0e8aeb71daa0937606996b521
lazydev01 <arpeetbhavsar01@gmail.com>
2023-12-05 06:27:04 +0530

    Codeforces To My Critics

---
+++ lazydev01/day_350.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef ONLINE_JUDGE
+  #define cerr if(0) cerr
+#endif
+
+#define int long long int
+#define endl "\n"
+#define YES cout<<"YES\n"
+#define NO cout<<"NO\n"
+
+void toMyCritics(){
+    int t;
+    cin >> t;
+    while(t--){
+        int a, b, c;
+        cin >> a >> b >> c;
+        vector<int> arr;
+        arr.push_back(a);
+        arr.push_back(b);
+        arr.push_back(c);
+        sort(arr.begin(), arr.end());
+        if(arr[1] + arr[2] >= 10){
+            cout << "YES" << endl;
+        }
+        else{
+            cout << "NO" << endl;
+        }
+    }
+}
+
+int32_t main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+
+    toMyCritics();
+    return 0;
+}
