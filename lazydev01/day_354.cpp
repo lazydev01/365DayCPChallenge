2bb0cbda6311308b97d1d3214b30e1f51b24e2ba
lazydev01 <arpeetbhavsar01@gmail.com>
2023-12-09 09:59:22 +0530

    Codechef Starters 111 Reach CodeTown

--- lazydev01/day_354.cpp
+++ lazydev01/day_354.cpp
@@ -25,11 +25,43 @@ void exams(){
     }
 }
 
+bool isVowel(char c){
+    vector<char> vowels = {'A', 'E', 'I', 'O', 'U'};
+    if(find(vowels.begin(), vowels.end(), c)!=vowels.end()){
+        return true;
+    }
+    return false;
+}
+
+void reachCodeTown(){
+    int t;
+    cin >> t;
+    while(t--){
+        string s;
+        cin >> s;
+        string c = "CODETOWN";
+        bool flag = false;
+        for(int i=0; i<8; i++){
+            if(!((isVowel(c[i]) && isVowel(s[i])) || (!isVowel(c[i]) && !isVowel(s[i])))){
+                flag = true;
+                break;
+            }
+        }
+        if(flag){
+            cout << "NO" << endl;
+        }
+        else{
+            cout << "YES" << endl;
+        }
+    }
+}
+
 int32_t main()
 {
 ios_base::sync_with_stdio(false);
 cin.tie(NULL);
 
-    exams();
+    // exams();
+    reachCodeTown();
     return 0;
 }
