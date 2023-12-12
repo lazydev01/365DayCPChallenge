d9d6031b6cdbee351c8bc029c4f6f36d54943a57
lazydev01 <arpeetbhavsar01@gmail.com>
2023-12-12 21:23:51 +0530

    Codechef Starters 111 Prefixing

--- lazydev01/day_357.cpp
+++ lazydev01/day_357.cpp
@@ -20,11 +20,50 @@ void airHockey(){
     }
 }
 
+void prefixing(){
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        vector<int> arr_a(n);
+        for(int i=0; i<n; i++){
+            cin >> arr_a[i];
+        }
+        vector<int> arr_b(n);
+        map<int, int> mp_a;
+        map<int, int> mp_b;
+        for(auto i : arr_a){
+            mp_a[i]++;
+        }
+        int largest = *max_element(arr_a.begin(), arr_a.end());
+        int ptr = 0;
+        for(int i=0; i<n; i++){
+            int curr = arr_a[i];
+            if(mp_b[curr]>0){
+                arr_b[i] = largest;
+                mp_b[largest]++;
+                mp_a[largest]--;
+            }
+            else{
+                arr_b[i] = curr;
+                mp_b[curr]++;
+                mp_a[curr]--;
+            }
+        }
+        for(auto i : arr_b){
+            cout << i << " ";
+        }
+        cout << endl;
+    }
+}
+
 int32_t main()
 {
 ios_base::sync_with_stdio(false);
 cin.tie(NULL);
 
-    airHockey();
+    // airHockey();
+    prefixing();
     return 0;
 }
