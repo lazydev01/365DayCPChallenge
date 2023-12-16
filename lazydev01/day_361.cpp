ea97c902b9659a1f4cde50bf8928917788e8e321
lazydev01 <arpeetbhavsar01@gmail.com>
2023-12-16 11:45:59 +0530

    Codechef Starters 112 Fair Share Settlement

--- lazydev01/day_361.cpp
+++ lazydev01/day_361.cpp
@@ -16,11 +16,26 @@ void foodCosts(){
     cout << (6*x) + y << endl;
 }
 
+void fairShareSettlement(){
+  int t;
+  cin >> t;
+  while(t--){
+    double n, k;
+    cin >> n >> k;
+    double ans = (ceil)(n/(k+1));
+    double left_amount = n - ans;
+    int each_pay = (float)(n/(k+1));
+    double extra_amount = left_amount - (each_pay * k);
+    cout << ans+extra_amount << endl;
+  }
+}
+
 int32_t main()
 {
 ios_base::sync_with_stdio(false);
 cin.tie(NULL);
 
-    foodCosts();    
+    // foodCosts();    
+    fairShareSettlement();
     return 0;
 }
