01ebcf62ab863e6cbc240d1c7bc5dd12648d0ba5
lazydev01 <arpeetbhavsar01@gmail.com>
2023-12-17 11:09:03 +0530

    Codechef Starters 112 3 Logicians Walk into a Bar

--- lazydev01/day_362.cpp
+++ lazydev01/day_362.cpp
@@ -35,11 +35,48 @@ void maxNutrition(){
     }
 }
 
+void threeLogiciansWalkIntoABbar(){
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        string s;
+        cin >> s;
+        vector<string> ans(n);
+        for(int i=0; i<n; i++){
+            if(i==0){
+                if(s[i]=='0'){
+                    ans[i] = "NO";
+                }
+                else{
+                    ans[i] = "IDK";
+                }
+            }
+            else{
+                if(s[i]=='0' || ans[i-1]=="NO"){
+                    ans[i] = "NO";
+                }
+                else{
+                    ans[i] = "IDK";
+                }
+            }
+        }
+        if(ans[n-1]=="IDK"){
+            ans[n-1] = "YES";
+        }
+        for(string s : ans){
+            cout << s << endl;
+        }
+    } 
+}
+
 int32_t main()
 {
 ios_base::sync_with_stdio(false);
 cin.tie(NULL);
 
-    maxNutrition();
+    // maxNutrition();
+    threeLogiciansWalkIntoABbar();
     return 0;
 }
