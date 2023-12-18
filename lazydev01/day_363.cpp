041816f1e74a552ca6ee82e898fe7c38d2e61fa3
lazydev01 <arpeetbhavsar01@gmail.com>
2023-12-18 07:19:08 +0530

    Codechef Starters 112 Cursed Indices

--- lazydev01/day_363.cpp
+++ lazydev01/day_363.cpp
@@ -31,11 +31,53 @@ void worms(){
     }
 }
 
+int linearSearch(vector<int> &arr, int ind, int n){
+    for(int i=ind; i<arr.size(); i++){
+        if(arr[i]>=n){
+            return i;
+        }
+    }
+    return -1;
+}
+
+void cursedIndices(){
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        vector<int> arr(n);
+        for(int i=0; i<n; i++){
+            cin >> arr[i];
+        }
+        sort(arr.begin(), arr.end());
+        int count = 0;
+        int sum = 0;
+        for(int i=0; i<n; i++){
+            sum += arr[i];
+            if(i<(n-1) && sum>=arr[i+1]){
+                int ind = linearSearch(arr, i+1, sum+1);
+                if(ind==-1){
+                    count = n-i-1;
+                    break;
+                }
+                swap(arr[i+1], arr[ind]);
+            }
+        }
+        cout << count << endl;
+        for(auto i : arr){
+            cout << i << " "; 
+        }
+        cout << endl;
+    }
+}
+
 int32_t main()
 {
 ios_base::sync_with_stdio(false);
 cin.tie(NULL);
 
-    worms();
+    // worms();
+    cursedIndices();
     return 0;
 }
