da39d927f4e45864e5992fdc3d2c58a63144b861
lazydev01 <arpeetbhavsar01@gmail.com>
2023-12-22 08:00:41 +0530

    Codechef Starters 112 Thala for a Reason

---
+++ lazydev01/day_367.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef ONLINE_JUDGE
+  #define cerr if(0) cerr
+#endif
+
+#define int long long int
+#define endl "\n"
+#define YES cout<<"YES\n"
+#define NO cout<<"NO\n"
+
+void thalaForAReason(){
+    int n;
+    cin >> n;
+    if(n==7){
+        cout << "THALA" << endl;
+    }
+    else{
+        cout << "SADGE" << endl;
+    }
+}
+
+int32_t main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+
+    thalaForAReason();
+    return 0;
+}
