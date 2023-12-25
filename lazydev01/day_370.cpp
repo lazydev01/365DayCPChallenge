ea35f63da6557e5309eae86e4da24b018b524070
lazydev01 <arpeetbhavsar01@gmail.com>
2023-12-25 21:23:26 +0530

    Codechef Starters 112 Yet Another Array Game

--- lazydev01/day_370.cpp
+++ lazydev01/day_370.cpp
@@ -40,11 +40,49 @@ void increasing(){
     }
 }
 
+void yetAnotherArrayGame(){
+    int t, s;
+    cin >> t >> s;
+    while(t--){
+        int n;
+        cin >> n;
+        vector<int> arr(n);
+        for(int i=0; i<n; i++){
+            cin >> arr[i];
+        }
+        int ans = 0;
+        sort(arr.begin(), arr.end());
+        if(s==0){
+            for(int i=1; i<=(n/2); i++){
+                ans += arr[i];
+            }
+            if(n%2){
+                ans += arr[0];
+            }            
+        }
+        else{
+            reverse(arr.begin(), arr.end());
+            vector<int> pre(n+1, 0);
+            for(int i=0; i<n; i++){
+                pre[i+1] = pre[i] + arr[i];
+            }
+            for(int i=0; i<n; i++){
+                if((2*i) + 1 > n){
+                    break;
+                }
+                ans = max(ans, pre[i+i+1] - pre[i]);
+            }
+        }
+        cout << ans << endl;
+    }
+}
+
 int32_t main()
 {
 ios_base::sync_with_stdio(false);
 cin.tie(NULL);
 
-    increasing();
+    // increasing();
+    yetAnotherArrayGame();
     return 0;
 }
