40ce6bae1e62f0ecd8dc26519719698acccf4400
lazydev01 <arpeetbhavsar01@gmail.com>
2023-12-28 06:55:03 +0530

    Codechef Starters 113 Make All Equal

--- lazydev01/day_373.cpp
+++ lazydev01/day_373.cpp
@@ -20,11 +20,37 @@ void aplusb(){
     }
 }
 
+void makeAllEqual(){
+    int t;
+    cin >> t;
+    while(t--){
+        int n, m;
+        cin >> n >> m;
+        vector<int> arr(n);
+        for(int i=0; i<n; i++){
+            cin >> arr[i];
+        }
+        int maxi = *max_element(arr.begin(), arr.end());
+        vector<int> b(n);
+        for(int i=0; i<n; i++){
+            b[i] = maxi - arr[i];
+        }
+        int cnt = 0;
+        for(int i=0; i<n; i++){
+            cnt += b[i];
+        }
+        int ans = *max_element(b.begin(), b.end());
+        int ans2 = (cnt + m - 1)/m;
+        cout << max(ans, ans2) << endl;
+    }
+}
+
 int32_t main()
 {
 ios_base::sync_with_stdio(false);
 cin.tie(NULL);
 
-    aplusb();
+    // aplusb();
+    makeAllEqual();
     return 0;
 }
