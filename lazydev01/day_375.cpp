61a780cd82d9deef2a46a7aab3ccb377abf0f7a8
lazydev01 <arpeetbhavsar01@gmail.com>
2023-12-30 07:55:57 +0530

    Codechef Starters 114 Christmas Candy

---
+++ lazydev01/day_375.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef ONLINE_JUDGE
+  #define cerr if(0) cerr
+#endif
+
+#define int long long int
+#define endl "\n"
+#define YES cout<<"YES\n"
+#define NO cout<<"NO\n"
+
+void christmasCandy(){
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        vector<int> arr(n);
+        for(int i=0; i<n; i++){
+            cin >> arr[i];
+        }
+        int maxi = arr[0];
+        int count = 0;
+        for(int i=1; i<n; i++){
+            if(arr[i]<maxi){
+                count++;
+            }
+            else{
+                maxi =  arr[i];
+            }
+        }
+        cout << count << endl;
+    }
+}
+
+int32_t main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+
+    christmasCandy();
+    return 0;
+}
