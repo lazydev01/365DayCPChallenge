463d5ac1df5e259122c6fc7fbd859e74d6b154fe
lazydev01 <arpeetbhavsar01@gmail.com>
2023-12-30 12:28:31 +0530

    Codechef Starters 114 Rock Paper Scissors

--- lazydev01/day_375.cpp
+++ lazydev01/day_375.cpp
@@ -34,11 +34,50 @@ void christmasCandy(){
     }
 }
 
+void rockPaperScissors(){
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        string s;
+        cin >> s;
+        string ans = "";
+        for(int i=0; i<n; i++){
+            ans += 'P';
+        }
+        int count = 0;
+        for(auto i : s){
+            if(i=='R'){
+                count++;
+            }
+        }
+        int left = (n/2) + 1 - count;
+        for(int i=n-1; i>=0; i--){
+            if(left<=0){
+                break;
+            }
+            if(s[i]=='R'){
+                continue;
+            }
+            else if(s[i]=='P'){
+                ans[i]='S';
+            }
+            else{
+                ans[i]='R';
+            }
+            left--;
+        }
+        cout << ans << endl;
+    }
+}
+
 int32_t main()
 {
 ios_base::sync_with_stdio(false);
 cin.tie(NULL);
 
-    christmasCandy();
+    // christmasCandy();
+    rockPaperScissors();
     return 0;
 }
