b04cdd6ab7b0848faf1af5f95fc2fe3ef52aa71d
lazydev01 <arpeetbhavsar01@gmail.com>
2024-01-04 08:18:50 +0530

    Codechef Starters 115 Greedy LIS Algorithm

--- lazydev01/day_380.cpp
+++ lazydev01/day_380.cpp
@@ -30,11 +30,82 @@ void arrayColoring(){
     }
 }
 
+void greedyLISAlgorithm(){
+    int t;
+    cin >> t;
+    while(t--){
+        int n, x;
+        cin >> n >> x;
+        vector<int> ans(n);
+        if(x==0){
+            for(int i=1; i<=n; i++){
+                ans[i-1] = i;
+            }
+            for(auto i : ans){
+                cout << i << " ";
+            }
+            cout << endl;
+        }
+        else if(x>n-2){
+            cout << -1 << endl;
+        }
+        else{
+            for(int i=1; i<=(n-(x+2)); i++){
+                ans[i-1] = i;
+            }
+            ans[n-(x+2)] = n;
+            for(int i=(n-(x+2))+1; i<=n-1; i++){
+                ans[i] = i;
+            }
+            for(auto i : ans){
+                cout << i << " ";
+            }
+            cout << endl;
+        }
+    }
+}
+
 int32_t main()
 {
 ios_base::sync_with_stdio(false);
 cin.tie(NULL);
 
-    arrayColoring();
+    // arrayColoring();
+    greedyLISAlgorithm();
     return 0;
 }
+
+/*
+
+5 0
+1 2 3 4 5
+
+5 1
+1 2 5 3 4 
+
+5 2
+1 5 2 3 4
+
+5 3
+5 1 2 3 4
+
+6 0
+1 2 3 4 5 6
+
+6 1
+1 2 3 6 4 5
+
+6 2
+1 2 6 3 4 5
+
+6 3
+1 6 2 3 4 5
+
+6 4
+6 1 2 3 4 5
+
+
+
+
+
+*/
