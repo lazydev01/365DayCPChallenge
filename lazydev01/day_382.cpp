9d825448271337518ff0b37b13435b1310b100e5
lazydev01 <arpeetbhavsar01@gmail.com>
2024-01-06 09:45:58 +0530

    Codeforces Good Kid

---
+++ lazydev01/day_382.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef ONLINE_JUDGE
+  #define cerr if(0) cerr
+#endif
+
+#define int long long int
+#define endl "\n"
+#define YES cout<<"YES\n"
+#define NO cout<<"NO\n"
+
+void goodKid(){
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        int mini = INT_MAX;
+        int index = -1;
+        vector<int> arr(n);
+        for(int i = 0; i < n; i++){
+            cin >> arr[i];
+            if(mini>arr[i]){
+                mini = arr[i];
+                index = i;
+            }
+        }
+        arr[index]++;
+        int ans = 1;
+        for(auto i : arr){
+            ans *= i;
+        }
+        cout << ans << endl;
+    }
+}
+
+int32_t main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+
+    goodKid();
+    return 0;
+}
