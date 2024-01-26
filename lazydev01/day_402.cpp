3f81f09d7767e6aa31739d6bf1aa426a9eb70854
lazydev01 <arpeetbhavsar01@gmail.com>
2024-01-26 15:58:35 +0530

    Codechef Starters 118 Literacy Rate

--- lazydev01/day_402.cpp
+++ lazydev01/day_402.cpp
@@ -16,11 +16,28 @@ void internationalEducationDay(){
     cout << max(a, b) * c << endl;
 }
 
+void literacyRate(){
+    int t;
+    cin >> t;
+    while(t--){
+        int p, l;
+        cin >> p >> l;
+        int literacyRate = (l/(p*1.0))*100;
+        if(literacyRate>=75){
+            cout << "YES" << endl;
+        }
+        else{
+            cout << "NO" << endl;
+        }
+    }
+}
+
 int32_t main()
 {
 ios_base::sync_with_stdio(false);
 cin.tie(NULL);
 
-    internationalEducationDay();
+    // internationalEducationDay();
+    literacyRate();
     return 0;
 }
