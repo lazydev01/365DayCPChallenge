40ed7689b603a3225b59eabde508fa9a0aeab60b
lazydev01 <arpeetbhavsar01@gmail.com>
2024-02-03 08:55:44 +0530

    Codechef Starters 119 Advitiya

---
+++ lazydev01/day_410.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef ONLINE_JUDGE
+  #define cerr if(0) cerr
+#endif
+
+#define int long long int
+#define endl "\n"
+#define YES cout<<"YES\n"
+#define NO cout<<"NO\n"
+
+void advitiya(){
+    int n;
+    cin >> n;
+    if(n>=16){
+        cout << "ADVITIYA" << endl;
+    }
+    else{
+        cout << "WAITING FOR ADVITIYA" << endl;
+    }
+}
+
+int32_t main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+
+    advitiya();
+    return 0;
+}
