58a875ae61a361517e85aabfc965b820c30b0154
lazydev01 <arpeetbhavsar01@gmail.com>
2024-02-10 12:39:17 +0530

    Starters 120 Valentine Gifts

--- lazydev01/day_417.cpp
+++ lazydev01/day_417.cpp
@@ -21,10 +21,26 @@ void valentinesContest(){
     }
 }
 
+void valentineGifts(){
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        if(n>=127){
+            cout << "YES" << endl;
+        }
+        else{
+            cout << "NO" << endl;
+        }
+    }
+}
+
 int32_t main()
 {
 ios_base::sync_with_stdio(false);
 cin.tie(NULL);
 
-    valentinesContest();
+    // valentinesContest();
+    valentineGifts();
 }
