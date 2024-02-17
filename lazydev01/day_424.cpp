a424f4b4238af83df0003ead02a0689ea9e19623
lazydev01 <arpeetbhavsar01@gmail.com>
2024-02-17 21:12:05 +0530

    Codechef Starters 121 Anti Adjacent Swaps

--- lazydev01/day_424.cpp
+++ lazydev01/day_424.cpp
@@ -26,12 +26,102 @@ void theManCode(){
   }
 }
 
+void antiAdjacentSwaps(){
+  int t;
+  cin >> t;
+  while(t--){
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for(int i=0; i<n; i++){
+      cin >> arr[i];
+    }
+    if(n==1 || n>=4){
+      cout << "YES" << endl;
+    }
+    else{
+      vector<int> arr_2 = arr;
+      sort(arr_2.begin(), arr_2.end());
+      if(arr[n/2] == arr_2[n/2]){
+        cout << "YES" << endl;
+      }
+      else{
+        cout << "NO" << endl;
+      }
+    }
+  }
+}
+
 int32_t main()
 {
 ios_base::sync_with_stdio(false);
 cin.tie(NULL);
 
     // legSpace();
-    theManCode();
+    // theManCode();
+    antiAdjacentSwaps();
     return 0;
 }
+
+/*
+
+4 3 2 1
+1 2 4 3
+
+4 2 1 3
+3 2 1 4
+
+1 2 3 4
+1 2 3 4 5
+1 5 2 4 3
+1 5 3 4 2
+1 2 3 4 5
+
+1 3 2 4
+3 1 2 4
+2 1 3 4
+2 4 3 1
+1 4 3 2
+1 2 3 4
+
+1 3 2
+2 3 1
+
+3 2 1
+
+1 2 3 4
+
+1 2 4 3
+3 2 4 1
+4 2 3 1
+1 2 3 4
+
+1 3 2 4
+1 3 4 2
+1 4 2 3
+1 4 3 2
+2 1 3 4
+2 1 4 3
+2 3 1 4
+2 3 4 1
+2 4 1 3
+2 4 3 1
+3 1 2 4
+3 1 4 2
+3 2 1 4
+3 2 4 1
+3 4 1 2
+3 4 2 1
+4 1 2 3
+4 1 3 2
+4 2 1 3
+4 2 3 1
+4 3 1 2
+4 3 2 1
+
+
+
+
+
+
+*/
