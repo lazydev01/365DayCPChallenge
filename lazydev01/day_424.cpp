a50800e78f68f6a4af5cf9e301fe1c0060841e89
lazydev01 <arpeetbhavsar01@gmail.com>
2024-02-17 12:35:52 +0530

    Codechef Starters 121 Leg Space

---
+++ lazydev01/day_424.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef ONLINE_JUDGE
+  #define cerr if(0) cerr
+#endif
+
+#define int long long int
+#define endl "\n"
+#define YES cout<<"YES\n"
+#define NO cout<<"NO\n"
+
+void legSpace(){
+    int n, m;
+    cin >> n >> m;
+    cout << (n==m ? "NO" : "YES") << endl;
+}
+
+int32_t main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+
+    legSpace();
+    return 0;
+}
