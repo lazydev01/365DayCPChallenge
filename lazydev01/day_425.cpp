50ee8a2b0e9c115702e592910e8b846ea3c23755
lazydev01 <arpeetbhavsar01@gmail.com>
2024-02-18 11:29:37 +0530

    Codechef Starters 121 Caesar Cipher Lite

---
+++ lazydev01/day_425.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef ONLINE_JUDGE
+  #define cerr if(0) cerr
+#endif
+
+#define int long long int
+#define endl "\n"
+#define YES cout<<"YES\n"
+#define NO cout<<"NO\n"
+
+void caesarCipherLite(){
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        string plainText, cipherText;
+        cin >> plainText >> cipherText;
+        for(int i=0; i<n; i++){
+            int p = plainText[i] - 'a' + 1;
+            int c = cipherText[i] - 'a' + 1;
+            int diff;
+            if(p>c){
+                diff = (26-p) + c;
+            }
+            else{
+                diff = c-p;
+            }
+            cout << (diff + ((diff%3)*26))/3 << " ";
+        }
+        cout << endl;
+    }
+}
+
+int32_t main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+
+    caesarCipherLite();
+    return 0;
+}
