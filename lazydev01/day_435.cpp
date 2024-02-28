1ff6c2b8fdf442a4c76680ee509386cec99bfa23
arpit <arpeetbhavsar01@gmail.com>
2024-02-28 07:44:11 +0530

    Codechef Starters 122 Transformation Cost

--- lazydev01/day_435.cpp
+++ lazydev01/day_435.cpp
@@ -25,7 +25,58 @@ void fileName(){
     cout << count << endl;
 }
 
+int calcPower2(int n){
+    int ans = 1;
+    for(int i=1; i<=n; i++){
+        ans = ans * 2;
+    }
+    return ans;
+}
+
+void transformationCost(){
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        int last_one_index = -1;
+        int log_n = log2(n);
+        int flag = 1;
+        int ans = 0;
+        bool flag2 = false;
+        for(int i=0; i<=log_n; i++){
+            if((n & flag)){
+                if(!flag2){
+                    last_one_index = i;
+                    flag2 = true;
+                }
+            }
+            else{
+                if(last_one_index!=-1){
+                    ans += calcPower2(last_one_index);
+                    last_one_index=i;
+                }
+            }
+            flag = flag << 1;
+        }
+        cout << ans << endl;
+
+    }
+}
+
 int32_t main(){
-    fileName();
+    // fileName();
+    transformationCost();
     return 0;
 }
+
+
+/*
+1010100011010111
+
+
+
+
+
+
+*/
