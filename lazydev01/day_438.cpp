df0568e1d8cb863e0d80bfe6b90458ce28e9aac5
lazydev01 <arpeetbhavsar01@gmail.com>
2024-03-02 12:35:20 +0530

    Codechef Starters 123 Algomaniac Finals

---
+++ lazydev01/day_438.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef ONLINE_JUDGE
+  #define cerr if(0) cerr
+#endif
+
+#define int long long int
+#define endl "\n"
+#define YES cout<<"YES\n"
+#define NO cout<<"NO\n"
+
+void alogmaniacFinals(){
+    int n;
+    cin >> n;
+    if(n==17){
+        cout << "YAY" << endl;
+    }
+    else{
+        cout << "NO" << endl;
+    }
+}
+
+int32_t main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+
+    alogmaniacFinals();
+    return 0;
+}
