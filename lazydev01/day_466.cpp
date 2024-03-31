40eb9dd8af7ba79f0821feffde17f589c43dc509
lazydev01 <arpeetbhavsar01@gmail.com>
2024-03-31 16:40:51 +0530

    Codechef Starters 127 ABC Conjecture

--- lazydev01/day_466.cpp
+++ lazydev01/day_466.cpp
@@ -29,11 +29,89 @@ void antiTriangle(){
     }
 }
 
+void abcConjecture(){
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        string a, b;
+        cin >> a >> b;
+        map<int, int> mp_a, mp_b;
+        for(char i : a){
+            mp_a[i]++;
+        }
+        for(char i : b){
+            mp_b[i]++;
+        }
+        bool flag = true;
+        for(char c = 'a'; c<='c'; c++){
+            if(mp_a[c]!=mp_b[c]){
+                flag = false;
+            }
+        }
+        if(!flag){
+            cout << "NO" << endl;
+            continue;
+        }
+        for(int i=0; i<n; i++){
+            if(a[i]=='b' && a[i]!=b[i]){
+                flag = false;
+                break;
+            }
+        }
+        if(!flag){
+            cout << "NO" << endl;
+            continue;
+        }
+        vector<int> misplaced_a_arr, misplaced_c_arr, b_arr;
+        for(int i=0; i<n; i++){
+            if(a[i]=='b'){
+                b_arr.push_back(i);
+            }
+            else if(a[i]!=b[i]){
+                if(a[i]=='a'){
+                    misplaced_a_arr.push_back(i);
+                }
+                else{
+                    misplaced_c_arr.push_back(i);
+                }
+            }
+        }
+        for(int i=0; i<misplaced_a_arr.size(); i++){
+            if(misplaced_a_arr[i] > misplaced_c_arr[i]){
+                flag = false;
+                break;
+            }
+            else{
+                int a_ind = lower_bound(b_arr.begin(), b_arr.end(), misplaced_a_arr[i])-b_arr.begin();
+                int c_ind = lower_bound(b_arr.begin(), b_arr.end(), misplaced_c_arr[i])-b_arr.begin();
+                if(a_ind>=c_ind){
+                    flag = false;
+                    break;
+                }
+            }
+        }
+        if(!flag){
+            cout << "NO" << endl;
+        }
+        else{
+            cout << "YES" << endl;
+        }
+    }
+}
+
 int32_t main()
 {
 ios_base::sync_with_stdio(false);
 cin.tie(NULL);
 
-    antiTriangle();    
+    // antiTriangle();
+    abcConjecture();    
     return 0;
 }
+
+// cabac
+// acbca
+
+// ccbaa
