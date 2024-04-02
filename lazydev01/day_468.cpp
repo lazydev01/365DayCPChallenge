1f8fe9b5219ff2e4cf8680f25a68c216b7211ceb
lazydev01 <arpeetbhavsar01@gmail.com>
2024-04-02 08:23:10 +0530

    Codeforces QAQ

--- lazydev01/day_468.cpp
+++ lazydev01/day_468.cpp
@@ -17,11 +17,34 @@ void chefAndInstantNoodles(){
     cout << x*y << endl;
 }
 
+void qaq(){
+  string s;
+  cin >> s;
+  int n = s.size();
+  vector<int> q_arr;
+  for(int i = 0; i < n; i++){
+    if(s[i]=='Q'){
+      q_arr.push_back(i);
+    }
+  }
+  int count = 0;
+  for(int i=0; i<n; i++){
+    if(s[i]=='A'){
+      int a_ind = upper_bound(q_arr.begin(), q_arr.end(), i) - q_arr.begin();
+      int left_q = a_ind;
+      int right_q = q_arr.size() - a_ind;
+      count+=(left_q * right_q);
+    }
+  }
+  cout << count << endl;
+}
+
 int32_t main()
 {
 ios_base::sync_with_stdio(false);
 cin.tie(NULL);
 
-    chefAndInstantNoodles();
+    // chefAndInstantNoodles();
+    qaq();
     return 0;
 }
