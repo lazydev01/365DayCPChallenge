bfe1d99ff164517a26881d02341fe5f629157845
lazydev01 <arpeetbhavsar01@gmail.com>
2024-04-02 06:47:12 +0530

    Codechef Chef and Instant Noodles

---
+++ lazydev01/day_468.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef ONLINE_JUDGE
+  #define cerr if(0) cerr
+#endif
+
+#define int long long int
+#define endl "\n"
+#define YES cout<<"YES\n"
+#define NO cout<<"NO\n"
+
+
+void chefAndInstantNoodles(){
+    int x, y;
+    cin >> x >> y;
+    cout << x*y << endl;
+}
+
+int32_t main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+
+    chefAndInstantNoodles();
+    return 0;
+}
