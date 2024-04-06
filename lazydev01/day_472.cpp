7ba9236e646442219f7565ece0a87b0a1457349e
lazydev01 <arpeetbhavsar01@gmail.com>
2024-04-06 18:14:49 +0530

    Codechef Starters 128 Moody Chef

--- lazydev01/day_472.cpp
+++ lazydev01/day_472.cpp
@@ -16,11 +16,37 @@ void reach5Star(){
     cout << ((x+y)>=2000 ? "YES" : "NO") << endl;
 }
 
+void moodyChef(){
+  int t;
+  cin >> t;
+  while(t--){
+    int n, l, r;
+    cin >> n >> l >> r;
+    vector<int> arr(n);
+    int max_el = 0;
+    int min_el = 0;
+    int happiness = 0;
+    for(int i=0; i<n; i++){
+      cin >> arr[i];
+      if(arr[i]>=l && arr[i]<=r){
+        happiness++;
+      }
+      else{
+        happiness--;
+      }
+      min_el = min(min_el, happiness);
+      max_el = max(max_el, happiness);
+    }
+    cout << max_el << " " << min_el << endl;
+  }
+}
+
 int32_t main()
 {
 ios_base::sync_with_stdio(false);
 cin.tie(NULL);
 
-    reach5Star();
+    // reach5Star();
+    moodyChef();
     return 0;
 }
