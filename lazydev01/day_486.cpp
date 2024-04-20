88cf9277d802240daae56592c4369f139ba29a26
lazydev01 <arpeetbhavsar01@gmail.com>
2024-04-20 21:40:57 +0530

    Codechef Starters 130 Swapping Marks Digits

--- lazydev01/day_486.cpp
+++ lazydev01/day_486.cpp
@@ -15,7 +15,25 @@ void giantWheel(){
     }
 }
 
+void swappingMarksDigits(){
+    int t;
+    cin >> t;
+    while(t--){
+        string a, b;
+        cin >> a >> b;
+        string reversed_a = string(a.rbegin(), a.rend());
+        string reversed_b = string(b.rbegin(), b.rend());
+        if(stoi(a)>stoi(b) || stoi(reversed_a)>stoi(b) || stoi(reversed_a)>stoi(reversed_b) || stoi(a)>stoi(reversed_b)){
+            cout << "YES" << endl;
+        }
+        else{
+            cout << "NO" << endl;
+        }
+    }
+}
+
 int32_t main(){
-    giantWheel();
+    // giantWheel();
+    swappingMarksDigits();
     return 0;
 }
