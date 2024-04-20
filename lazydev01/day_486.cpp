aae104f7081e760db7458a93309cad1fbd37fce2
arpit <arpeetbhavsar01@gmail.com>
2024-04-20 15:29:08 +0530

    Codechef Starters 130 Giant Wheel

---
+++ lazydev01/day_486.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define int long long int
+#define endl "\n"
+
+void giantWheel(){
+    int n;
+    cin >> n;
+    if(n>=60){
+        cout << "YES" << endl;
+    }
+    else{
+        cout << "NO" << endl;
+    }
+}
+
+int32_t main(){
+    giantWheel();
+    return 0;
+}
