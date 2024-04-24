53c05066a8b302ae4247ddc661d26e68edcbf687
lazydev01 <arpeetbhavsar01@gmail.com>
2024-04-24 07:51:37 +0530

    Codechef Starters 130 Make Equal

--- lazydev01/day_490.cpp
+++ lazydev01/day_490.cpp
@@ -25,11 +25,33 @@ void walletExchange(){
     }
 }
 
+void makeEqual(){
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        vector<int> arr(n);
+        int mini = INT_MAX;
+        for(int i=0; i<n; i++){
+            cin >> arr[i];
+            mini = min(mini, arr[i]);
+        }
+        if(arr[0]==arr[n-1] && arr[0]==mini){
+            cout << "YES" << endl;
+        }
+        else{
+            cout << "NO" << endl;
+        }
+    }
+}
+
 int32_t main()
 {
 ios_base::sync_with_stdio(false);
 cin.tie(NULL);
 
-    walletExchange();
+    // walletExchange();
+    makeEqual();
     return 0;
 }
