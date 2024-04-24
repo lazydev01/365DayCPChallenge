c79b7b44d95df0f521a32e1b7d8ca49674dac23a
lazydev01 <arpeetbhavsar01@gmail.com>
2024-04-24 06:21:45 +0530

    Codeforces Wallet Exchange

---
+++ lazydev01/day_490.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef ONLINE_JUDGE
+  #define cerr if(0) cerr
+#endif
+
+#define int long long int
+#define endl "\n"
+#define YES cout<<"YES\n"
+#define NO cout<<"NO\n"
+
+void walletExchange(){
+    int t;
+    cin >> t;
+    while(t--){
+        int a, b;
+        cin >> a >> b;
+        if((a+b)%2){
+            cout << "Alice" << endl;
+        }
+        else{
+            cout << "Bob" << endl;
+        }
+    }
+}
+
+int32_t main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+
+    walletExchange();
+    return 0;
+}
