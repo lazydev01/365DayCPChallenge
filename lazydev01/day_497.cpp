82bebcf9d15767b3fc479112c06fae9c5b58cc6b
lazydev01 <arpeetbhavsar01@gmail.com>
2024-05-01 07:10:36 +0530

    Codechef Starters 131 k-closeness

--- lazydev01/day_497.cpp
+++ lazydev01/day_497.cpp
@@ -25,11 +25,32 @@ void twoThreeMoves(){
     }
 }
 
+void kCloseness(){
+    int t;
+    cin >> t;
+    while(t--){
+        int n, k;
+        cin >> n >> k;
+        vector<int> arr(n);
+        for(int i=0; i<n; i++){
+            cin >> arr[i];
+            arr[i]=arr[i]%k;
+        }
+        sort(arr.begin(), arr.end());
+        int ans = arr[n-1]-arr[0];
+        for(int i=0; i<n-1; i++){
+            ans = min(ans, arr[i]-arr[i+1]+k);
+        }
+        cout << ans << endl;
+    }
+}
+
 int32_t main()
 {
 ios_base::sync_with_stdio(false);
 cin.tie(NULL);
 
-    twoThreeMoves();
+    // twoThreeMoves();
+    kCloseness();
     return 0;
 }
