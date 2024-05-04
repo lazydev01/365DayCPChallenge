c8c130d22ee66f1db5dea0a2848309f0d128210d
lazydev01 <arpeetbhavsar01@gmail.com>
2024-05-04 15:49:29 +0530

    Codechef Starters 132 Game XI

--- lazydev01/day_500.cpp
+++ lazydev01/day_500.cpp
@@ -21,11 +21,58 @@ void iceCream(){
     }
 }
 
+void gameXi(){
+    int t;
+    cin >> t;
+    while(t--){
+        int n, m;
+        cin >> n >> m;
+        vector<int> batsman(n);
+        vector<int> bowler(m);
+        for(int i=0; i<n; i++){
+            cin >> batsman[i];
+        }
+        for(int i=0; i<m; i++){
+            cin >> bowler[i];
+        }
+        if(n<4 || m<4){
+            cout << -1 << endl;
+            continue;
+        }
+        if(n+m<11){
+            cout << -1 << endl;
+            continue;
+        }
+        sort(batsman.begin(), batsman.end(), greater<int>());
+        sort(bowler.begin(), bowler.end(), greater<int>());
+        vector<int> final_list;
+        for(int i=4; i<batsman.size(); i++){
+            final_list.push_back(batsman[i]);
+        }
+        for(int i=4; i<bowler.size(); i++){
+            final_list.push_back(bowler[i]);
+        }
+        sort(final_list.begin(), final_list.end(), greater<int>());
+        int ans = 0;
+        for(int i=0; i<4; i++){
+            ans+=batsman[i];
+        }
+        for(int i=0; i<4; i++){
+            ans+=bowler[i];
+        }
+        for(int i=0; i<3; i++){
+            ans+=final_list[i];
+        }
+        cout << ans << endl;
+    }
+}
+
 int32_t main()
 {
 ios_base::sync_with_stdio(false);
 cin.tie(NULL);
 
-    iceCream();
+    // iceCream();
+    gameXi();
     return 0;
 }
