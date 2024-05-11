c865c583de8507fe58ab51ee7155ecc8dc471d84
lazydev01 <arpeetbhavsar01@gmail.com>
2024-05-11 13:10:45 +0530

    Codeforces Buttons

--- lazydev01/day_507.cpp
+++ lazydev01/day_507.cpp
@@ -56,11 +56,22 @@ void stripes(){
     }
 }
 
+void buttons(){
+    int n;
+    cin >> n;
+    int ans = n;
+    for(int i=2; i<=n; i++){
+        ans += (((n-i)*i)+1);
+    }
+    cout << ans << endl;
+}
+
 int32_t main()
 {
 ios_base::sync_with_stdio(false);
 cin.tie(NULL);
 
-    stripes();
+    // stripes();
+    buttons();
     return 0;
 }
