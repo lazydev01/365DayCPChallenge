e668a5774499e85be695f30ce20cd0fdec2287a6
lazydev01 <arpeetbhavsar01@gmail.com>
2024-05-11 17:54:03 +0530

    Codeforces Buying Shovels

--- lazydev01/day_507.cpp
+++ lazydev01/day_507.cpp
@@ -66,12 +66,42 @@ void buttons(){
     cout << ans << endl;
 }
 
+vector<int> factorize(int n){
+    vector<int> ans;
+    for(int i=1; i*i<=n; i++){
+        if(n%i==0){
+            if(n/i == i){
+                ans.push_back(i);
+            }
+            else{
+                ans.push_back(i);
+                ans.push_back(n/i);
+            }
+        }
+    }
+    sort(ans.begin(), ans.end());
+    return ans;
+}
+
+void buyingShovels(){
+    int t;
+    cin >> t;
+    while(t--){
+        int n, k;
+        cin >> n >> k;
+        vector<int> arr = factorize(n);
+        int index = upper_bound(arr.begin(), arr.end(), k) - arr.begin() - 1;
+        cout << n/arr[index] << endl;
+    }
+}
+
 int32_t main()
 {
 ios_base::sync_with_stdio(false);
 cin.tie(NULL);
 
     // stripes();
-    buttons();
+    // buttons();
+    buyingShovels();
     return 0;
 }
