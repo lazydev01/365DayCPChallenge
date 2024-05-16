a53a78ab84696eac0f4aa0933c4116d2bf576234
lazydev01 <arpeetbhavsar01@gmail.com>
2024-05-16 08:22:13 +0530

    Codechef Starters 134 Rock Paper Scissors

--- lazydev01/day_512.cpp
+++ lazydev01/day_512.cpp
@@ -20,11 +20,39 @@ void myFirstSortingProblem(){
     }
 }
 
+void rockPaperScissors(){
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        string s;
+        cin >> s;
+        bool flag = false;
+        int ans = 1;
+        for(int i = 0; i < n-1;){
+            if(flag){
+                flag = false;
+                ans++;
+            }
+            else if(s[i]==s[i+1]){
+                flag = true;
+            }
+            else{
+                ans++;
+            }
+            i++;
+        }
+        cout << ans << endl;
+    }
+}
+
 int32_t main()
 {
 ios_base::sync_with_stdio(false);
 cin.tie(NULL);
 
-    myFirstSortingProblem();
+    // myFirstSortingProblem();
+    rockPaperScissors();
     return 0;
 }
