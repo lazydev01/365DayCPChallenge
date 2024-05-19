5bcae85668614befce1cf3eee1164b1ad07b602d
lazydev01 <arpeetbhavsar01@gmail.com>
2024-05-19 07:52:41 +0530

    Codeforces Following the String

---
+++ lazydev01/day_515.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef ONLINE_JUDGE
+  #define cerr if(0) cerr
+#endif
+
+#define int long long int
+#define endl "\n"
+#define YES cout<<"YES\n"
+#define NO cout<<"NO\n"
+
+void followingTheString(){
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        vector<int> arr(n);
+        map<int, queue<char>> mp;
+        for(int i=0; i<n; i++) cin >> arr[i];
+        for(char i='a'; i<='z'; i++){
+            mp[0].push(i);
+        }
+        string ans = "";
+        for(int i=0; i<arr.size(); i++){
+            ans+=mp[arr[i]].front();
+            mp[arr[i]+1].push(mp[arr[i]].front());
+            mp[arr[i]].pop();
+        }
+        cout << ans << endl;
+    }
+}
+
+int32_t main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+
+    followingTheString();
+    return 0;
+}
