6b66387731276340917a54e374e04260d2bdeacd
lazydev01 <arpeetbhavsar01@gmail.com>
2024-05-20 08:16:54 +0530

    Codeforces Make Equal

--- lazydev01/day_516.cpp
+++ lazydev01/day_516.cpp
@@ -31,11 +31,41 @@ void juicer(){
     cout << ans << endl;
 }
 
+void makeEqual(){
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        vector<int> arr(n);
+        int sum = 0;
+        for(int i=0; i<n; i++){
+            cin >> arr[i];
+            sum+=arr[i];
+        }
+        int avg = sum/n;
+        int checker = 0;
+        for(int i=n-1; i>=0; i--){
+            if(checker>0){
+                break;
+            }
+            checker+=(arr[i]-avg);
+        }
+        if(checker>0){
+            cout << "NO" << endl;
+        }
+        else{
+            cout << "YES" << endl;
+        }
+    }
+}
+
 int32_t main()
 {
 ios_base::sync_with_stdio(false);
 cin.tie(NULL);
 
-    juicer();
+    // juicer();
+    makeEqual();
     return 0;
 }
