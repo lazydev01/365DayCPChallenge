ba96b7f8a23640f8eb308ef36294b8e34c47f9af
lazydev01 <arpeetbhavsar01@gmail.com>
2024-05-22 06:27:19 +0530

    Codechef Starters 133 Devouring Donuts

---
+++ lazydev01/day_518.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef ONLINE_JUDGE
+  #define cerr if(0) cerr
+#endif
+
+#define int long long int
+#define endl "\n"
+#define YES cout<<"YES\n"
+#define NO cout<<"NO\n"
+
+void devouringDonuts(){
+    int x, y;
+    cin >> x >> y;
+    cout << x*y << endl;
+}
+
+int32_t main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+
+    devouringDonuts();
+    return 0;
+}
