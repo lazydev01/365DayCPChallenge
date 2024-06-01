b30828f13b8eb5cc6d474aaf128b02e1f3068e68
lazydev01 <arpeetbhavsar01@gmail.com>
2024-06-01 13:07:36 +0530

    Codeforces String LCM

--- lazydev01/day_528.cpp
+++ lazydev01/day_528.cpp
@@ -32,11 +32,44 @@ void minimumVariedNumber(){
     }
 }
 
+int gcd(int a, int b) 
+{ 
+  if (b == 0) 
+    return a; 
+  return gcd(b, a % b); 
+} 
+
+void stringLCM(){
+    int t;
+    cin >> t;
+    while(t--){
+        string s, t;
+        cin >> s >> t;
+        int len_s = s.size();
+        int len_t = t.size();
+        int len_lcm = (len_s * len_t)/gcd(len_s, len_t);
+        string s1 = "", s2 = "";
+        for(int i=0; i<len_lcm/len_s; i++){
+            s1 += s;
+        }
+        for(int i=0; i<len_lcm/len_t; i++){
+            s2 += t;
+        }
+        if(s1==s2){
+            cout << s1 << endl;
+        }
+        else{
+            cout << -1 << endl;
+        }
+    }
+}
+
 int32_t main()
 {
 ios_base::sync_with_stdio(false);
 cin.tie(NULL);
 
-    minimumVariedNumber();
+    // minimumVariedNumber();
+    stringLCM();
     return 0;
 }
