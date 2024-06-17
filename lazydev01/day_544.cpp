7b9cbab7d5ac036da029acba1b125a0cc3777d0b
lazydev01 <arpeetbhavsar01@gmail.com>
2024-06-17 21:33:30 +0530

    SSDE Next Permutation

--- lazydev01/day_544.cpp
+++ lazydev01/day_544.cpp
@@ -24,11 +24,47 @@ void sashaAndTheBeautifulArray(){
     }
 }
 
+void nextPermutation(){
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for(int i=0; i<n; i++){
+        cin >> arr[i];
+    }
+    int index = -1;
+    for(int i=n-1; i>0; i--){
+        if(arr[i] > arr[i-1]){
+            index = i-1;
+            break;
+        }
+    }
+    if(index== -1){
+        sort(arr.begin(), arr.end());
+    }
+    else{
+        for(int i=n-1; i>index; i--){
+            if(arr[i]>arr[index]){
+                int temp = arr[index];
+                arr[index] = arr[i];
+                arr[i] = temp;
+                break;
+            }
+        }
+        sort(arr.begin()+index+1, arr.end());
+    }
+    for(int i=0; i<n; i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+}
+
 int32_t main()
 {
 ios_base::sync_with_stdio(false);
 cin.tie(NULL);
 
-    sashaAndTheBeautifulArray();
+    // sashaAndTheBeautifulArray();
+    nextPermutation();
     return 0;
 }
