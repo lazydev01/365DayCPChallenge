f9e66550c5ae4b7cadd544e44f62c91b4bb2be1e
lazydev01 <arpeetbhavsar01@gmail.com>
2024-06-17 06:46:41 +0530

    Codeforces Sasha and the Beautiful Array

---
+++ lazydev01/day_544.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef ONLINE_JUDGE
+  #define cerr if(0) cerr
+#endif
+
+#define int long long int
+#define endl "\n"
+#define YES cout<<"YES\n"
+#define NO cout<<"NO\n"
+
+void sashaAndTheBeautifulArray(){
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        vector<int> arr(n);
+        for(int i=0; i<n; i++){
+            cin >> arr[i];
+        }
+        cout << *max_element(arr.begin(), arr.end()) - *min_element(arr.begin(), arr.end()) << endl;;
+    }
+}
+
+int32_t main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+
+    sashaAndTheBeautifulArray();
+    return 0;
+}
