4df18e70afd47f10b3b5ee150616a380debb774b
lazydev01 <arpeetbhavsar01@gmail.com>
2024-06-19 08:02:26 +0530

    Codeforces Sequence Game

---
+++ lazydev01/day_546.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef ONLINE_JUDGE
+  #define cerr if(0) cerr
+#endif
+
+#define int long long int
+#define endl "\n"
+#define YES cout<<"YES\n"
+#define NO cout<<"NO\n"
+
+void sequenceGame(){
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        vector<int> arr(n);
+        for(int i=0; i<n; i++){
+            cin >> arr[i];
+        }
+        vector<int> ans;
+        ans.push_back(arr[0]);
+        for(int i=1; i<n; i++){
+            if(arr[i-1] <= arr[i]){
+                ans.push_back(arr[i]);
+            }
+            else{
+                ans.push_back(arr[i]);
+                ans.push_back(arr[i]);
+            }
+        }
+        cout << ans.size() << endl;
+        for(int i=0; i<ans.size(); i++){
+            cout << ans[i] << " ";
+        }
+        cout << endl;
+    }
+}
+
+int32_t main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+
+    sequenceGame();
+    return 0;
+}
