9a3889131d4e35c7b8941b8dd94a06348daab730
lazydev01 <arpeetbhavsar01@gmail.com>
2024-06-26 08:31:51 +0530

    SSDE Merge Overlapping SubIntervals

--- lazydev01/day_553.cpp
+++ lazydev01/day_553.cpp
@@ -24,11 +24,50 @@ void xAxis(){
     }
 }
 
+bool sortAccordingToFirstEl(pair<int, int> &first, pair<int, int> &second){
+    if(first.first < second.first) return true;
+    else if(first.first == second.first) return first.second < second.second;
+    return false;
+}
+
+void mergeOverlappingSubIntervals(){
+    int n;
+    cin >> n;
+    vector<pair<int, int>> vp(n);
+    for(int i=0; i<n; i++){
+        int a, b;
+        cin >> a >> b;
+        vp[i] = make_pair(a, b);
+    }
+    sort(vp.begin(), vp.end(), sortAccordingToFirstEl);
+    int ind = 1;
+    int start = vp[0].first;
+    int end = vp[0].second;
+    vector<pair<int, int>> ans;
+    while(ind < n){
+        if(vp[ind].first <= end){
+            start = min(start, vp[ind].first);
+            end = max(end, vp[ind].second);
+        }
+        else{
+            ans.push_back(make_pair(start, end));
+            start = vp[ind].first;
+            end = vp[ind].second;
+        }
+        ind++;
+    }
+    ans.push_back(make_pair(start, end));
+    for(int i=0; i<ans.size(); i++){
+        cout << ans[i].first << " " << ans[i].second << endl;
+    }
+}
+
 int32_t main()
 {
 ios_base::sync_with_stdio(false);
 cin.tie(NULL);
 
-    xAxis();
+    // xAxis();
+    mergeOverlappingSubIntervals();
     return 0;
 }
