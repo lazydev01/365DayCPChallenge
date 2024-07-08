5e055d7da978efcb7f5807ded572ffbb4a804cf6
lazydev01 <arpeetbhavsar01@gmail.com>
2024-07-08 08:39:03 +0530

    SSDE Search in a Sorted 2D Matrix

--- lazydev01/day_564.cpp
+++ lazydev01/day_564.cpp
@@ -51,11 +51,83 @@ void findAndReplace(){
     }
 }
 
+void searchInASorted2DMatrix(){
+    int n, m;
+    cin >> n >> m;
+    vector<vector<int>> mat(n, vector<int>(m));
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            cin >> mat[i][j];
+        }
+    }
+    int target;
+    cin >> target;
+    int row = -1;
+    bool flag = false;
+    for(int i=0; i<n; i++){
+        if(mat[i][0] == target){
+            flag = true;
+            break;
+        }
+        if(target > mat[i][0]){
+            row = i;
+        }
+        else{
+            break;
+        }
+    }
+    if(flag){
+        cout << flag << endl;
+        return;
+    }
+    int ind = binary_search(mat[row].begin(), mat[row].end(), target);
+    if(ind){
+        cout << true << endl;
+    }
+    else{
+        cout << false << endl;
+    }
+}
+
+void searchInASorted2DMatrixOptimal(){
+    int n, m;
+    cin >> n >> m;
+    vector<vector<int>> mat(n, vector<int>(m));
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            cin >> mat[i][j];
+        }
+    }
+    int target;
+    cin >> target;
+    int low = 0;
+    int high = (n*m - 1);
+    bool flag = false;
+    while(low<=high){
+        int mid = (low + high) / 2;
+        int row = mid/m;
+        int col = mid%m;
+        if(mat[row][col] == target){
+            flag = true;
+            break;
+        }
+        else if(mat[row][col] < target){
+            low = mid + 1;
+        }
+        else{
+            high = mid - 1;
+        }
+    }
+    cout << flag << endl;
+}
+
 int32_t main()
 {
 ios_base::sync_with_stdio(false);
 cin.tie(NULL);
 
-    findAndReplace();
+    // findAndReplace();
+    // searchInASorted2DMatrix();
+    searchInASorted2DMatrixOptimal();
     return 0;
 }
