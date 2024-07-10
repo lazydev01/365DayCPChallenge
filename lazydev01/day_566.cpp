6769403dcd97c34eaf596c3bcb2121cd03306e34
lazydev01 <arpeetbhavsar01@gmail.com>
2024-07-10 06:45:23 +0530

    Codechef Lucky Clover

---
+++ lazydev01/day_566.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef ONLINE_JUDGE
+  #define cerr if(0) cerr
+#endif
+
+#define int long long int
+#define endl "\n"
+#define YES cout<<"YES\n"
+#define NO cout<<"NO\n"
+
+void luckyClover(){
+    int n;
+    cin >> n;
+    cout << 4 + (3 * (n-1)) << endl;
+}
+
+int32_t main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+
+    luckyClover();
+    return 0;
+}
