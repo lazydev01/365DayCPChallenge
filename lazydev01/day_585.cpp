c503eb7178d93a76a5d8b8547fbba586304b275f
lazydev01 <arpeetbhavsar01@gmail.com>
2024-07-29 07:44:01 +0530

    SSDE Count Number of Subarrays with Given XOR K

--- lazydev01/day_585.cpp
+++ lazydev01/day_585.cpp
@@ -26,11 +26,39 @@ void angryMonk(){
     }
 }
 
+void countNumberOfSubarraysWithGivenXorK(){
+    int n, k;
+    cin >> n >> k;
+    vector<int> arr(n+1);
+    arr.push_back(0);
+    for(int i=1; i<=n; i++){
+        cin >> arr[i];
+        arr[i] = arr[i] ^ arr[i-1];
+    }
+    unordered_map<int, int> mp;
+    int ans = 0;
+    
+    for(int i=0; i<=n; i++){
+        ans += mp[arr[i]^k];
+        mp[arr[i]]++;
+    }   
+    cout << ans << endl;
+}
+
 int32_t main()
 {
 ios_base::sync_with_stdio(false);
 cin.tie(NULL);
 
-    angryMonk();
+    // angryMonk();
+    countNumberOfSubarraysWithGivenXorK();
     return 0;
 }
+
+/*
+
+4 2 2 6 4
+4 6 4 2 6
+
+
+*/
