bd3a8715a9be7162e6203fa79012cfe80bab6107
lazydev01 <arpeetbhavsar01@gmail.com>
2024-08-08 06:51:44 +0530

    SSDE Find Middle Element of LinkedList

--- lazydev01/day_593.cpp
+++ lazydev01/day_593.cpp
@@ -34,11 +34,67 @@ void goodPrefixes(){
     }
 }
 
+
+struct ListNode{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr){}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+ListNode* initializeLinkedList(int n, vector<int> &arr){
+    ListNode* head = NULL;
+    ListNode* node = NULL;
+    for(int i = 0; i < n; i++){
+        ListNode* curr = new ListNode(arr[i]);
+        if(!head){
+            head = curr;
+            node = curr;
+        }
+        else{
+            head->next = curr;
+            head = head->next;
+        }
+    }
+    return node;
+}
+
+
+void printLinkedList(ListNode* node){
+    while(node != NULL){
+        cout << node->val << endl;
+        node = node->next;
+    }
+}
+
+void findMiddleElementOfLinkedList(){
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for(int i=0; i<n; i++){
+        cin >> arr[i];
+    }
+    ListNode* ll = initializeLinkedList(n, arr);
+    int count = 0;
+    ListNode* head = ll;
+    while(ll!=NULL){
+        ll = ll->next;
+        count++;
+    }
+    int toMove = count / 2;
+    while(toMove--){
+        head = head->next;
+    }
+    printLinkedList(head);
+}
+
 int32_t main()
 {
 ios_base::sync_with_stdio(false);
 cin.tie(NULL);
 
-    goodPrefixes();
+    // goodPrefixes();
+    findMiddleElementOfLinkedList();
     return 0;
 }
