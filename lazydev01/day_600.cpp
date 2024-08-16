3aa71277a758705320cd09b76c70d13e08309178
lazydev01 <arpeetbhavsar01@gmail.com>
2024-08-16 06:54:01 +0530

    Codeforces Primary Task

---
+++ lazydev01/day_600.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef ONLINE_JUDGE
+  #define cerr if(0) cerr
+#endif
+
+#define int long long int
+#define endl "\n"
+#define YES cout<<"YES\n"
+#define NO cout<<"NO\n"
+
+void primaryTask(){
+    int t;
+    cin >> t;
+    while(t--){
+        string s;
+        cin >> s;
+        if(s.substr(0, 2)!="10"){
+            cout << "NO" << endl;
+        }
+        else{
+            if(s.substr(2, s.size()-2)[0]=='0' || stoi(s.substr(2, s.size()-2)) < 2){
+                cout << "NO" << endl;
+            }
+            else{
+                cout << "YES" << endl;
+            }
+        }
+    }
+}
+
+int32_t main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+
+    primaryTask();
+    return 0;
+}
