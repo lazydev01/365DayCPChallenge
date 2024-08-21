9258ae098b061f4d3ca7db60dc6063603a4bcc12
lazydev01 <arpeetbhavsar01@gmail.com>
2024-08-21 06:16:04 +0530

    Codeforces Halloumi Boxes

---
+++ lazydev01/day_605.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef ONLINE_JUDGE
+  #define cerr if(0) cerr
+#endif
+
+#define int long long int
+#define endl "\n"
+#define YES cout<<"YES\n"
+#define NO cout<<"NO\n"
+
+void halloumiBoxes(){
+    int t;
+    cin >> t;
+    while(t--){
+        int n, k;
+        cin >> n >> k;
+        vector<int> arr(n);
+        for(int i=0; i<n; i++){
+            cin >> arr[i];
+        }
+        bool isSorted = true;
+        for(int i=1; i<n; i++){
+            if(arr[i]<arr[i-1]){
+                isSorted = false;
+                break;
+            }
+        }
+        if(!isSorted && k==1){
+            cout << "NO" << endl;
+        }
+        else{
+            cout << "YES" << endl;
+        }
+    }
+}
+
+int32_t main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+
+    halloumiBoxes();
+    return 0;
+}
