1b2b5a96e608568f31521c1501709a8d0594a8f4
lazydev01 <arpeetbhavsar01@gmail.com>
2024-09-04 06:40:49 +0530

    SSDE Trapping Rainwater

--- lazydev01/day_618.cpp
+++ lazydev01/day_618.cpp
@@ -21,11 +21,39 @@ void minimize(){
     }
 }
 
+void rainwaterProblem(){
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for(int i=0; i<n; i++){
+        cin >> arr[i];
+    }
+    int l = 0;
+    int r = n-1;
+    int leftMax = 0;
+    int rightMax = 0;
+    int ans = 0;
+    while(l<r){
+        if(arr[l] < arr[r]){
+            ans += (max(leftMax-arr[l], 0ll));
+            leftMax = max(leftMax, arr[l]);
+            l++;
+        }
+        else{
+            ans += (max(rightMax-arr[r], 0ll));
+            rightMax = max(rightMax, arr[r]);
+            r--;
+        }
+    }
+    cout << ans << endl;
+}
+
 int32_t main()
 {
 ios_base::sync_with_stdio(false);
 cin.tie(NULL);
 
-    minimize();
+    // minimize();
+    rainwaterProblem();
     return 0;
 }
