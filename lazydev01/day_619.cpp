a7b6a179d6c04bd32eaafd779a235b5a601f14ae
lazydev01 <arpeetbhavsar01@gmail.com>
2024-09-05 06:29:50 +0530

    Codeforces Bad Boy

---
+++ lazydev01/day_619.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef ONLINE_JUDGE
+  #define cerr if(0) cerr
+#endif
+
+#define int long long int
+#define endl "\n"
+#define YES cout<<"YES\n"
+#define NO cout<<"NO\n"
+
+void badBoy(){
+    int t;
+    cin >> t;
+    while(t--){
+        int n, m, i, j;
+        cin >> n >> m >> i >> j;
+        cout << 1 << " " << 1 << " " << n << " " << m << endl;
+    }
+}
+
+int32_t main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+
+    badBoy();
+    return 0;
+}
