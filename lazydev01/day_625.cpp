7e48d4f30485d4ca52277b34ba537fffe75a5424
lazydev01 <arpeetbhavsar01@gmail.com>
2024-09-12 06:39:00 +0530

    SSDE Max Consecutive Ones

--- lazydev01/day_625.cpp
+++ lazydev01/day_625.cpp
@@ -37,11 +37,33 @@ void symmetricEncoding(){
     }
 }
 
+void maxConsecutiveOnes(){
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for(int i=0; i<n; i++){
+        cin >> arr[i];
+    }
+    int maxCount = 0;
+    int cnt = 0;
+    for(int i=0; i<n; i++){
+        if(arr[i]==1){
+            cnt++;
+        }
+        else{
+            cnt = 0;
+        }
+        maxCount = max(maxCount, cnt);
+    }
+    cout << maxCount << endl;
+}
+
 int32_t main()
 {
 ios_base::sync_with_stdio(false);
 cin.tie(NULL);
 
-    symmetricEncoding();
+    // symmetricEncoding();
+    maxConsecutiveOnes();
     return 0;
 }
