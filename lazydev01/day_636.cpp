b629f035f15bf87b85cc1953bb963f79055eb275
lazydev01 <arpeetbhavsar01@gmail.com>
2024-09-25 08:16:57 +0530

    SSDE Fractional Knapsack

--- lazydev01/day_636.cpp
+++ lazydev01/day_636.cpp
@@ -35,11 +35,50 @@ void comparisonStrings(){
     }
 }
 
+bool compare(pair<double, int> &p1, pair<double, int> &p2){
+  if(p1.first > p2.first){
+    return true;
+  }
+  return false;
+}
+
+void fractionalKnapsack(){
+  int n, w;
+  cin >> n >> w;
+  vector<double> values(n);
+  vector<int> weights(n);
+  for(int i=0; i<n; i++){
+    cin >> values[i];
+  }
+  for(int i=0; i<n; i++){
+    cin >> weights[i];
+  }
+  for(int i=0; i<n; i++){
+    values[i] = values[i]/weights[i];
+  }
+  vector<pair<double, int>> vp;
+  for(int i=0; i<n; i++){
+    vp.push_back(make_pair(values[i], weights[i]));
+  }
+  sort(vp.begin(), vp.end(), compare);
+  double totalSum = 0;
+  for(auto i : vp){
+    if(w==0){
+      break;
+    }
+    int weight = min(i.second, w);
+    totalSum+=(weight * i.first);
+    w-=weight;
+  }
+  cout << totalSum << endl;
+}
+
 int32_t main()
 {
 ios_base::sync_with_stdio(false);
 cin.tie(NULL);
 
-    comparisonStrings();
+    // comparisonStrings();
+    fractionalKnapsack();
     return 0;
 }
