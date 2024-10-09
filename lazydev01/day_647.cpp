e184ec5b560eef69058b44b2ffbbef34f33f0b91
lazydev01 <arpeetbhavsar01@gmail.com>
2024-10-09 08:56:07 +0530

    Codeforces Parity Sort

---
+++ lazydev01/day_647.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef ONLINE_JUDGE
+  #define cerr if(0) cerr
+#endif
+
+#define int long long int
+#define endl "\n"
+#define YES cout<<"YES\n"
+#define NO cout<<"NO\n"
+
+void paritySort(){
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        vector<int> arr(n);
+        for(int i=0; i<n; i++){
+            cin >> arr[i];
+        }
+        vector<int> b = arr;
+        sort(b.begin(), b.end());
+        bool flag = false;
+        for(int i=0; i<n; i++){
+            if(b[i]%2!=arr[i]%2){
+                flag = true;
+                break;
+            }
+        }
+        if(flag){
+            cout << "NO" << endl;
+        }
+        else{
+            cout << "YES" << endl;
+        }
+    }
+}
+
+int32_t main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+
+    paritySort();
+    return 0;
+}
