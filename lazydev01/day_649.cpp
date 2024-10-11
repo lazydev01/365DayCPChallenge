a183b0aee419234335b013e91257528796b6850b
lazydev01 <arpeetbhavsar01@gmail.com>
2024-10-11 06:49:35 +0530

    Codeforces Closest Point

---
+++ lazydev01/day_649.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef ONLINE_JUDGE
+  #define cerr if(0) cerr
+#endif
+
+#define int long long int
+#define endl "\n"
+#define YES cout<<"YES\n"
+#define NO cout<<"NO\n"
+
+void closestPoint(){
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        vector<int> arr(n);
+        for(int i=0; i<n; i++){
+            cin >> arr[i];
+        }
+        if(n==2){
+            if(arr[0]==arr[1]-1){
+                cout << "NO" << endl;
+            }
+            else{
+                cout << "YES" << endl;
+            }
+        }
+        else{
+            cout << "NO" << endl;
+        }
+    }
+}
+
+int32_t main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+
+    closestPoint();
+    return 0;
+}
