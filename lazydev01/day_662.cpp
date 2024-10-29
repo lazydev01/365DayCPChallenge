94303cb633641158226eae47c206bf0a5f4956f7
lazydev01 <arpeetbhavsar01@gmail.com>
2024-10-29 09:30:05 +0530

    SSDE kth Permutation Sequence

--- lazydev01/day_662.cpp
+++ lazydev01/day_662.cpp
@@ -30,11 +30,68 @@ void makeAllEqual(){
     }
 }
 
+void kthPermutationSequence(){
+    int n, k;
+    cin >> n >> k;
+    string ans = "";
+    int fact = 1;
+    vector<int> numbers;
+    for(int i=1; i<=n; i++){
+        fact = fact * i;
+        numbers.push_back(i);
+    }
+    k--;
+    fact = fact / n;
+    while(numbers.size()!=0){
+        ans = ans + to_string(numbers[k/fact]);
+        numbers.erase(numbers.begin() + (k/fact));
+        k = k%fact;
+        if(numbers.size()!=0){
+            fact = fact / numbers.size();
+        }
+    }
+    cout << ans << endl;
+}
+
 int32_t main()
 {
 ios_base::sync_with_stdio(false);
 cin.tie(NULL);
 
-    makeAllEqual();
+    // makeAllEqual();
+    kthPermutationSequence();
     return 0;
 }
+
+/*
+
+1234
+1243
+1324
+1342
+1423
+1432
+
+2134
+2143
+2314
+2341
+2413
+2431
+
+3124
+3142
+3214
+3241
+3412
+3421
+
+4123
+4132
+4213
+4231
+4312
+4321
+
+
+*/
