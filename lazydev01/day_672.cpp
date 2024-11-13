19f92efa5841e27cc5c819550cccd911d60b2e1d
lazydev01 <arpeetbhavsar01@gmail.com>
2024-11-13 06:28:06 +0530

    SSDE M Coloring Problem

--- lazydev01/day_672.cpp
+++ lazydev01/day_672.cpp
@@ -45,11 +45,55 @@ void longLong(){
     }
 }
 
+bool canColor(int vertex, map<int, vector<int>> &mp, map<int, int> &colorMap, int v, int m){
+    if(vertex==v+1){
+        return true;
+    }
+    for(int i=1; i<=m; i++){
+        bool hasColoredNeighbor = false;
+        for(int j=0; j<mp[vertex].size(); j++){
+            if(colorMap[mp[vertex][j]]==i){
+                hasColoredNeighbor = true;
+            }
+        }
+        if(!hasColoredNeighbor){
+            colorMap[vertex] = i;
+            if(canColor(vertex+1, mp, colorMap, v, m)) return true;
+            colorMap[vertex] = 0;
+        }
+    }
+    return false;
+}
+
+void mColoring(){
+    int v, m, e;
+    cin >> v >> m >> e;
+    vector<pair<int, int>> graph; 
+    for(int i=0; i<e; i++){
+        int v1, v2;
+        cin >> v1 >> v2;
+        graph.push_back(make_pair(v1+1, v2+1));
+    }
+    map<int, vector<int>> mp;
+    for(auto i : graph){
+        mp[i.first].push_back(i.second);
+        mp[i.second].push_back(i.first);
+    }
+    map<int, int> colorMap;
+    if(canColor(1, mp, colorMap, v, m)){
+        cout << "Yes" << endl;
+    }
+    else{
+        cout << "No" << endl;
+    }
+}
+
 int32_t main()
 {
 ios_base::sync_with_stdio(false);
 cin.tie(NULL);
 
-    longLong();
+    // longLong();
+    mColoring();
     return 0;
 }
