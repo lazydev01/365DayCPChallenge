ced399c9e128ab3621724ae785a6803611a816a1
lazydev01 <arpeetbhavsar01@gmail.com>
2024-11-19 07:04:28 +0530

    SSDE Work Break 2

--- lazydev01/day_677.cpp
+++ lazydev01/day_677.cpp
@@ -36,11 +36,43 @@ void quintomania(){
     }
 }
 
+void findValidSentences(int index, string currWord, string currSentence, string word, vector<string> dictionary, vector<string> &ans){
+    if(index==word.size()){
+        if(currWord==""){
+            ans.push_back(currSentence.substr(0, currSentence.size()-1));
+        }
+        return;
+    }
+    string tempCurrWord = currWord;
+    currWord += word[index];
+    if(find(dictionary.begin(), dictionary.end(), currWord)!=dictionary.end()){
+        findValidSentences(index+1, "", currSentence + currWord + " ", word, dictionary, ans);
+    }
+    findValidSentences(index+1, currWord, currSentence, word, dictionary, ans);
+}
+
+void wordBreak2(){
+    int n;
+    cin >> n;
+    vector<string> dictionary(n);
+    for(int i=0; i<n; i++){
+        cin >> dictionary[i];
+    }
+    string word;
+    cin >> word;
+    vector<string> ans;
+    findValidSentences(0, "", "", word, dictionary, ans);
+    for(int i=0; i<ans.size(); i++){
+        cout << ans[i] << endl;
+    }
+}
+
 int32_t main()
 {
 ios_base::sync_with_stdio(false);
 cin.tie(NULL);
 
-    quintomania();
+    // quintomania();
+    wordBreak2();
     return 0;
 }
