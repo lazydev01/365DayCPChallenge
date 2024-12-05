fd39ca7672903cf7c7c46010546a282913c11c1b
lazydev01 <arpeetbhavsar01@gmail.com>
2024-12-05 09:21:46 +0530

    SSDE Search Element In A Rotated Sorted Array

--- lazydev01/day_691.cpp
+++ lazydev01/day_691.cpp
@@ -28,11 +28,49 @@ void weGotEverythingCovered(){
     }
 }
 
+void searchElementInARotatedSortedArray(){
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for(int i=0; i<n; i++){
+        cin >> arr[i];
+    }
+    int k;
+    cin >> k;
+    int low = 0;
+    int high = n-1;
+    int ans = -1;
+    while(low<=high){
+        int mid = (low + high) / 2;
+        if(arr[mid]==k){
+            ans = mid;
+        }
+        if(arr[low] <= arr[mid]){
+            if(k>=arr[low] && k<=arr[mid]){
+                high = mid - 1;
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+        else{
+            if(k>=arr[mid] && k<=arr[high]){
+                low = mid + 1;
+            }
+            else{
+                high = mid - 1;
+            }
+        }
+    }
+    cout << ans << endl;
+}
+
 int32_t main()
 {
 ios_base::sync_with_stdio(false);
 cin.tie(NULL);
 
-    weGotEverythingCovered();
+    // weGotEverythingCovered();
+    searchElementInARotatedSortedArray();
     return 0;
 }
