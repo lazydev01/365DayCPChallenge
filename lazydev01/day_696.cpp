cf70a5c35c0f7dcbf613bed23dce48c027631423
lazydev01 <arpeetbhavsar01@gmail.com>
2024-12-11 07:10:55 +0530

    SSDE Kth Element of Two Sorted Arrays

--- lazydev01/day_696.cpp
+++ lazydev01/day_696.cpp
@@ -36,11 +36,65 @@ void mrPerfectlyFine(){
     }
 }
 
+void kthElementOfTwoSortedArrays(){
+    int n, m;
+    cin >> n >> m;
+    vector<int> a(n);
+    vector<int> b(m);
+    for(int i=0; i<n; i++){
+        cin >> a[i];
+    }
+    for(int i=0; i<m; i++){
+        cin >> b[i];
+    }
+    int k;
+    cin >> k;
+    if(n>m){
+        swap(n, m);
+        swap(a, b);
+    }
+    int low = max(0ll, k-n);
+    int high = min(k, n);
+    int ans;
+    while(low<=high){
+        int mid = (low + high)/2;
+        int mid2 = k - mid;
+        int l1 = INT_MIN;
+        int l2 = INT_MIN;
+        int r1 = INT_MAX;
+        int r2 = INT_MAX;
+        if(mid>0){
+            l1 = a[mid-1];
+        }
+        if(mid2>0){
+            l2 = b[mid2-1];
+        }
+        if(mid<n){
+            r1 = a[mid];
+        }
+        if(mid2<m){
+            r2 = b[mid2];
+        }
+        if(l1<=r2 && l2<=r1){
+            ans = max(l1, l2);
+            break;
+        }
+        else if(l1>r2){
+            high = mid - 1;
+        }
+        else{
+            low = mid + 1;
+        }
+    }
+    cout << ans << endl;
+}
+
 int32_t main()
 {
 ios_base::sync_with_stdio(false);
 cin.tie(NULL);
 
-    mrPerfectlyFine();
+    // mrPerfectlyFine();
+    kthElementOfTwoSortedArrays();
     return 0;
 }
