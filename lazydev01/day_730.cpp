22f062e90a26fed20e80f60e98bb20aff1149735
lazydev01 <arpeetbhavsar01@gmail.com>
2025-01-27 06:12:52 +0530

    Codeforces Longest Divisors Interval

---
+++ lazydev01/day_730.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef ONLINE_JUDGE
+  #define cerr if(0) cerr
+#endif
+
+#define int long long int
+#define endl "\n"
+#define YES cout<<"YES\n"
+#define NO cout<<"NO\n"
+
+void longestDivisorsInterval(){
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        int l = 1;
+        while(n%l==0){
+            l++;
+        }
+        cout << l-1 << endl;
+    }
+}
+
+int32_t main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+
+    longestDivisorsInterval();
+    return 0;
+}
