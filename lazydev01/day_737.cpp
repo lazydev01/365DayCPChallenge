4b95c3a0bfd43dcc889ce6019b5e3abfe0efba1a
lazydev01 <arpeetbhavsar01@gmail.com>
2025-02-05 07:11:28 +0530

    SSDE Merge K Sorted Arrays

--- lazydev01/day_737.cpp
+++ lazydev01/day_737.cpp
@@ -55,12 +55,51 @@ void decoding(){
     cout << ans << endl;
 }
 
+void mergeKSortedArrays(){
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        vector<vector<int>> arrays;
+        priority_queue< pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>> > heap;
+        for(int i=0; i<n; i++){
+            int m;
+            cin >> m;
+            vector<int> arr(m);
+            for(int j=0; j<m; j++){
+                cin >> arr[j];
+            }
+            heap.push(make_pair(arr[0], make_pair(0, i)));
+            arrays.push_back(arr);
+        }
+        vector<int> ans;
+        while(!heap.empty()){
+            pair<int, pair<int, int>> p = heap.top();
+            pair<int, int> meta = p.second;
+            int element = p.first;
+            int elIndex = meta.first;
+            int arrIndex = meta.second;
+            ans.push_back(element);
+            heap.pop();
+            if(elIndex < arrays[arrIndex].size()-1){
+                heap.push(make_pair(arrays[arrIndex][elIndex+1], make_pair(elIndex+1, arrIndex)));
+            }
+        }
+        for(int i=0; i<ans.size(); i++){
+            cout << ans[i] << " ";
+        }
+        cout << endl;
+    }
+}
+
 int32_t main()
 {
 ios_base::sync_with_stdio(false);
 cin.tie(NULL);
 
-    decoding();
+    // decoding();
+    mergeKSortedArrays();
     return 0;
 }
 
