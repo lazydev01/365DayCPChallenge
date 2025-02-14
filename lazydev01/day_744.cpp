79e208c46ae24bae4e374430b0b6e75d3e784a52
lazydev01 <arpeetbhavsar01@gmail.com>
2025-02-14 07:38:10 +0530

    SSDE Top K Frequent Elements

--- lazydev01/day_744.cpp
+++ lazydev01/day_744.cpp
@@ -33,11 +33,76 @@ void prefiquence(){
     }
 }
 
+bool cmp(pair<int, int> &a, pair<int, int> &b){
+    if(a.second > b.second){
+        return true;
+    }
+    return false;
+}
+
+void topKFrequentElements(){
+    int n, k;
+    cin >> n >> k;
+    vector<int> arr(n);
+    map<int, int> mp;
+    for(int i=0; i<n; i++){
+        cin >> arr[i];
+        mp[arr[i]]++;
+    }
+    vector<pair<int, int>> vp;
+    for(auto i : mp){
+        vp.push_back(i);
+    }
+    sort(vp.begin(), vp.end(), cmp);
+    vector<int> ans;
+    for(int i=0; i<k; i++){
+        ans.push_back(vp[i].first);
+    }
+    for(int i=0; i<ans.size(); i++){
+        cout << ans[i] << " ";
+    }
+    cout << endl;
+}
+
+void topKFrequentElementsOptimal(){
+    int n, k;
+    cin >> n >> k;
+    vector<int> arr(n);
+    map<int, int> mp;
+    for(int i=0; i<n; i++){
+        cin >> arr[i];
+        mp[arr[i]]++;
+    }
+    vector<vector<int>> freq(n+1, vector<int>());
+    for(auto i : mp){
+        freq[i.second].push_back(i.first);
+    }
+    vector<int> ans;
+    for(int i=freq.size()-1; i>=0; i--){
+        for(int j=0; j<freq[i].size(); j++){
+            ans.push_back(freq[i][j]);
+            k--;
+            if(k==0){
+                break;
+            }
+        }
+        if(k==0){
+            break;
+        }
+    }
+    for(int i=0; i<ans.size(); i++){
+        cout << ans[i] << " ";
+    }
+    cout << endl;
+}
+
 int32_t main()
 {
 ios_base::sync_with_stdio(false);
 cin.tie(NULL);
 
-    prefiquence();
+    // prefiquence();
+    // topKFrequentElements();
+    topKFrequentElementsOptimal();
     return 0;
 }
