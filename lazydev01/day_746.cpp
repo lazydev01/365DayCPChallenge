21f2917ba0097cd3f14b1fa0d817676d89078813
lazydev01 <arpeetbhavsar01@gmail.com>
2025-02-18 06:55:50 +0530

    Codefroces Jagged Swaps

---
+++ lazydev01/day_746.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef ONLINE_JUDGE
+  #define cerr if(0) cerr
+#endif
+
+#define int long long int
+#define endl "\n"
+#define YES cout<<"YES\n"
+#define NO cout<<"NO\n"
+
+void jaggedSwaps(){
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        vector<int> arr(n);
+        for(int i=0; i<n; i++){
+            cin >> arr[i];
+        }
+        int minEl = *min_element(arr.begin(), arr.end());
+        if(minEl == arr[0]){
+            cout << "YES" << endl;
+        }
+        else{
+            cout << "NO" << endl;
+        }
+    }
+}
+
+int32_t main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+
+    jaggedSwaps();
+    return 0;
+}
