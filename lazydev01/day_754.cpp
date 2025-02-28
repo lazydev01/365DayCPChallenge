207d26058cdcd1123401341191eca0c8e30c63d1
lazydev01 <arpeetbhavsar01@gmail.com>
2025-02-28 07:02:42 +0530

    SSDE Implement Queue using Array

--- lazydev01/day_754.cpp
+++ lazydev01/day_754.cpp
@@ -31,6 +31,33 @@ void gamingForces(){
     }
 }
 
+// Implement Queue using Array
+
+class MyQueue {
+    private:
+        int arr[100005];
+        int front;
+        int rear;
+    
+    public :
+        MyQueue(){front=0;rear=0;}
+        void push(int);
+        int pop();
+};
+
+// Function to push an element x in a queue.
+void MyQueue ::push(int x) {
+    arr[rear++] = x;
+}
+
+// Function to pop an element from queue and return that element.
+int MyQueue ::pop() {
+    if(front==rear){
+        return -1;
+    }
+    return arr[front++];
+}
+
 int32_t main()
 {
 ios_base::sync_with_stdio(false);
