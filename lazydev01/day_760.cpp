072b0be3e0691a0a49b981bcefae8b7c5397b742
lazydev01 <arpeetbhavsar01@gmail.com>
2025-03-08 07:06:50 +0530

    SSDE Implement Stack using Queue

---
+++ lazydev01/day_760.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class MyStack {
+    public:
+        queue<int> q;
+        MyStack() {
+        }
+        
+        void push(int x) {
+            q.push(x);
+            for(int i=0; i<q.size()-1; i++){
+                q.push(q.front());
+                q.pop();
+            }
+        }
+        
+        int pop() {
+            int firstEl = q.front();
+            q.pop();
+            return firstEl;
+        }
+        
+        int top() {
+            return q.front();
+        }
+        
+        bool empty() {
+            return q.size() == 0;
+        }
+    };
+    
+    /**
+     * Your MyStack object will be instantiated and called as such:
+     * MyStack* obj = new MyStack();
+     * obj->push(x);
+     * int param_2 = obj->pop();
+     * int param_3 = obj->top();
+     * bool param_4 = obj->empty();
+     */
