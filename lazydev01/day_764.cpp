5bcc9120e63cb21cee7eeb3ff764043e86327bc9
lazydev01 <arpeetbhavsar01@gmail.com>
2025-03-13 07:15:02 +0530

    SSDE Implement Queue using Stacks

--- lazydev01/day_764.cpp
+++ lazydev01/day_764.cpp
@@ -34,6 +34,47 @@ void vladAndASumOfSumOfDigits(){
     }
 }
 
+// Implement Queue using Stack
+
+class MyQueue {
+public:
+    stack<int> input, output;
+    
+    MyQueue() {
+        
+    }
+    
+    void push(int x) {
+        input.push(x);
+    }
+    
+    int pop() {
+        if(output.empty()){
+            while(!input.empty()){
+                output.push(input.top());
+                input.pop();
+            }
+        }
+        int firstEl = output.top();
+        output.pop();
+        return firstEl;
+    }
+    
+    int peek() {
+        if(output.empty()){
+            while(!input.empty()){
+                output.push(input.top());
+                input.pop();
+            }
+        }
+        return output.top();
+    }
+    
+    bool empty() {
+        return output.size() + input.size() == 0;
+    }
+};
+
 int32_t main()
 {
 ios_base::sync_with_stdio(false);
