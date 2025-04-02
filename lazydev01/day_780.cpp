302063f3c98fefbcfd1f15e5a2b16504c0f6b84a
lazydev01 <arpeetbhavsar01@gmail.com>
2025-04-02 07:04:03 +0530

    SSDE Nearest Smaller Element

--- lazydev01/day_780.cpp
+++ lazydev01/day_780.cpp
@@ -22,11 +22,39 @@ void ambitiousKid(){
     cout << minEl << endl;
 }
 
+void nearestSmallerElement(){
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for(int i=0; i<n; i++){
+        cin >> arr[i];
+    }
+    stack<int> s;
+    vector<int> ans;
+    for(int i=0; i<n; i++){
+        while(!s.empty() && s.top()>=arr[i]){
+            s.pop();
+        }
+        if(s.empty()){
+            ans.push_back(-1);
+        }
+        else{
+            ans.push_back(s.top());
+        }
+        s.push(arr[i]);
+    }
+    for(int i=0; i<n; i++){
+        cout << ans[i] << " ";
+    }
+    cout << endl;
+}
+
 int32_t main()
 {
 ios_base::sync_with_stdio(false);
 cin.tie(NULL);
 
-    ambitiousKid();
+    // ambitiousKid();
+    nearestSmallerElement();
     return 0;
 }
