5a43e01440894644f4e11480e83933c1cfdb4282
lazydev01 <arpeetbhavsar01@gmail.com>
2025-05-19 06:56:48 +0530

    Leetcode Next Greater Element 1

--- lazydev01/day_800.cpp
+++ lazydev01/day_800.cpp
@@ -37,6 +37,29 @@ void weNeedTheZero(){
     }
 }
 
+vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+    stack<int> s;
+    int n2 = nums2.size();
+    map<int, int> mp;
+    for(int i=n2-1; i>=0; i--){
+        while(!s.empty() && s.top() <= nums2[i]){
+            s.pop();
+        }
+        if(s.empty()){
+            mp[nums2[i]] = -1;
+        }
+        else{
+            mp[nums2[i]] = s.top();
+        }
+        s.push(nums2[i]);
+    }
+    vector<int> ans;
+    for(auto i : nums1){
+        ans.push_back(mp[i]);
+    }
+    return ans;
+}
+
 int32_t main()
 {
 ios_base::sync_with_stdio(false);
