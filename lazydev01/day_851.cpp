5ac87af63db18618310e404b10d6762f569987e4
lazydev01 <arpeetbhavsar01@gmail.com>
2025-07-29 06:26:38 +0530

    Codeforces Ezzat and Two Subsequences

---
+++ lazydev01/day_851.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef ONLINE_JUDGE
+  #define cerr if(0) cerr
+#endif
+
+#define int long long int
+#define endl "\n"
+#define YES cout<<"YES\n"
+#define NO cout<<"NO\n"
+
+void ezzatAndTwoSubsequences(){
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        vector<int> arr(n);
+        int maxElement = INT_MIN;
+        int sum = 0;
+        for(int i = 0; i < n; i++){
+            cin >> arr[i];
+            maxElement = max(maxElement, arr[i]);
+            sum+=arr[i];
+        }
+        double ans = (maxElement + ((sum - maxElement) / ((n - 1) * 1.0)));
+        cout << fixed << setprecision(9) << ans << endl;
+    }
+}
+
+int32_t main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+
+    ezzatAndTwoSubsequences();
+    return 0;
+}
