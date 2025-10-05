973ec3dcb4d02d501cd79026666740d6ca647877
lazydev01 <arpeetbhavsar01@gmail.com>
2025-10-05 08:37:25 +0530

    SSDE Preorder Traversal

---
+++ lazydev01/day_896.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+ struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  };
+class Solution {
+public:
+
+    void preOrderT(TreeNode* root, vector<int> &ans){
+        if(root == NULL) return;
+        ans.push_back(root->val);
+        if(root->left != NULL){
+            preOrderT(root->left, ans);
+        }
+        if(root->right != NULL){
+            preOrderT(root->right, ans);
+        }
+    }
+
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        preOrderT(root, ans);
+        return ans;
+    }
+};
