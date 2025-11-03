f1f33856b3d482653fda969cc40655595fe33e66
lazydev01 <arpeetbhavsar01@gmail.com>
2025-11-03 06:49:49 +0530

    SSDE Root to Node Path in Binary Tree

--- lazydev01/day_917.cpp
+++ lazydev01/day_917.cpp
@@ -35,11 +35,61 @@ void lasers(){
     }
 }
 
+struct TreeNode {
+    TreeNode* left;
+    TreeNode* right;
+    int val;
+    TreeNode(TreeNode* l, TreeNode* r, int data) : left(l), right(r), val(data) {}
+    TreeNode(int data) : left(NULL), right(NULL), val(data) {}
+};
+
+bool hasPath(TreeNode* node, int targetValue, vector<int> &ans){
+    if(node == NULL){
+        return false;
+    }
+    ans.push_back(node->val);
+    if (node->val == targetValue)    
+        return true;
+    if(hasPath(node->left, targetValue, ans) || hasPath(node->right, targetValue, ans)){
+        return true;
+    }
+    ans.pop_back();
+    return false;
+}
+
+vector<int> getRootToNodePath(TreeNode* root, int targetValue){
+    vector<int> ans;
+    if(hasPath(root, targetValue, ans)){
+        for(int i = 0; i < ans.size(); i++){
+            cout << ans[i];
+            if(i < ans.size() - 1){
+                cout << " -> ";
+            }
+        }
+        cout << endl;
+    }
+    else{
+        cout << "No Path found" << endl;
+    }
+}
+
 int32_t main()
 {
 ios_base::sync_with_stdio(false);
 cin.tie(NULL);
-
-    lasers();
+    TreeNode* one = new TreeNode(1);
+    TreeNode* two = new TreeNode(2);
+    TreeNode* three = new TreeNode(3);
+    TreeNode* four = new TreeNode(4);
+    TreeNode* five = new TreeNode(5);
+    TreeNode* six = new TreeNode(6);
+    TreeNode* seven = new TreeNode(7);
+    one->left = two;
+    one->right = three;
+    two->left = four;
+    two->right = five;
+    three->left = six;
+    three->right = seven;
+    getRootToNodePath(one, 7);
     return 0;
 }
