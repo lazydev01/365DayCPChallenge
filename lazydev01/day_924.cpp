8b90ac38961adfa43e44d5bdd23f628cb5cead24
lazydev01 <arpeetbhavsar01@gmail.com>
2025-11-13 06:46:34 +0530

    SSDE Max Width of Binary Tree

--- lazydev01/day_924.cpp
+++ lazydev01/day_924.cpp
@@ -24,6 +24,61 @@ void password(){
     }
 }
 
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    int widthOfBinaryTree(TreeNode* root) {
+        if(root == NULL) return 0;
+        int ans = 0;
+        queue<pair<TreeNode*, int>> q;
+        q.push(make_pair(root, 0));
+        while(!q.empty()){
+            int size = q.size();
+            int mini = q.front().second;
+            int first, last;
+            for(int i = 0; i < size; i++){
+                int curr = q.front().second - mini;
+                TreeNode* node = q.front().first;
+                q.pop();
+                if(i == 0){
+                    first = curr;
+                }
+                if(i == size - 1){
+                    last = curr;
+                }
+                if(node->left != NULL){
+                    q.push(make_pair(node->left, curr * 2 + 1));
+                }
+                if(node->right != NULL){
+                    q.push(make_pair(node->right, curr * 2 + 2));
+                }
+            }
+            ans = max(ans, last - first + 1);
+        }
+        return ans;
+    }
+};
+
 int32_t main()
 {
 ios_base::sync_with_stdio(false);
