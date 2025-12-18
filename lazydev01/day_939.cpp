507f8872ebc9a0774fdc8b36d9d9f7e4579612f7
lazydev01 <arpeetbhavsar01@gmail.com>
2025-12-18 07:00:19 +0530

    SSDE Check if binary tree is height-balanced or not

--- lazydev01/day_939.cpp
+++ lazydev01/day_939.cpp
@@ -48,6 +48,51 @@ cin.tie(NULL);
     return 0;
 }
 
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  };
+
+class Solution {
+public:
+
+    int calcHeight(TreeNode* node){
+        if(node == NULL){
+            return 0;
+        }
+        int leftHeight = calcHeight(node->left);
+        int rightHeight = calcHeight(node->right);
+        if(leftHeight == -1 || rightHeight == -1) return -1;
+        if(abs(leftHeight - rightHeight) > 1) return -1;
+        return max(leftHeight, rightHeight) + 1;
+    }
+
+    bool isBalanced(TreeNode* root) {
+        if(calcHeight(root) == -1){
+            return false;
+        }
+        return true;
+    }
+};
+
+
+
 /*
 
 10 4 1
