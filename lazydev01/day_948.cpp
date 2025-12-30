c9444bb026b1de4b37c34ddc7b2e300b98cfaa2b
lazydev01 <arpeetbhavsar01@gmail.com>
2025-12-30 06:50:04 +0530

    SSDE Check if two trees are identical or not

--- lazydev01/day_948.cpp
+++ lazydev01/day_948.cpp
@@ -66,3 +66,43 @@ cin.tie(NULL);
 
 
 */
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+
+    bool compare(TreeNode* p, TreeNode* q){
+        if(p==NULL && q==NULL){
+            return true;
+        }
+        if(p!=NULL && q!=NULL && p->val == q->val){
+            return compare(p->left, q->left) && compare(p->right, q->right);
+        }
+        return false;
+    }
+
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        return compare(p, q);
+    }
+};
