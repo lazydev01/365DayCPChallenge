4535a67e41656a0124ef8b827b4c6bd28ed32cae
lazydev01 <arpeetbhavsar01@gmail.com>
2026-02-09 06:51:20 +0530

    Codeforces Review Site

---
+++ lazydev01/day_979.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef ONLINE_JUDGE
+  #define cerr if(0) cerr
+#endif
+
+#define int long long int
+#define endl "\n"
+#define YES cout<<"YES\n"
+#define NO cout<<"NO\n"
+
+void reviewSite(){
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        vector<int> arr(n);
+        int ans = 0;
+        for(int i = 0; i < n; i++){
+            cin >> arr[i];
+            if(arr[i] != 2) ans++;
+        }
+        cout << ans << endl;
+    }
+}
+
+int32_t main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+
+    reviewSite();
+    return 0;
+}
