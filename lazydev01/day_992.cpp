01a923d7b2fd3ae5eeb50db720a42bc1e363987b
lazydev01 <arpeetbhavsar01@gmail.com>
2026-03-02 07:02:56 +0530

    SSDE Construct a BST from a preorder traversal

--- lazydev01/day_992.cpp
+++ lazydev01/day_992.cpp
@@ -2,37 +2,47 @@
 using namespace std;
 
 #ifdef ONLINE_JUDGE
-  #define cerr if(0) cerr
+#define cerr \
+    if (0)   \
+    cerr
 #endif
 
 #define int long long int
 #define endl "\n"
-#define YES cout<<"YES\n"
-#define NO cout<<"NO\n"
+#define YES cout << "YES\n"
+#define NO cout << "NO\n"
 
-void newPalindrome(){
+void newPalindrome()
+{
     int t;
     cin >> t;
-    while(t--){
+    while (t--)
+    {
         string s;
         cin >> s;
         map<char, int> mp;
-        for(auto i : s){
+        for (auto i : s)
+        {
             mp[i]++;
         }
         int count = 0;
-        for(auto i : mp){
-            if(count > 1){
+        for (auto i : mp)
+        {
+            if (count > 1)
+            {
                 break;
             }
-            if(i.second > 1){
+            if (i.second > 1)
+            {
                 count++;
             }
         }
-        if(count > 1){
+        if (count > 1)
+        {
             cout << "YES" << endl;
-        }    
-        else{
+        }
+        else
+        {
             cout << "NO" << endl;
         }
     }
@@ -40,9 +50,65 @@ void newPalindrome(){
 
 int32_t main()
 {
-ios_base::sync_with_stdio(false);
-cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 
     newPalindrome();
     return 0;
 }
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    TreeNode *createBST(vector<int> &preorder, int start, int end)
+    {
+        if (start > end)
+        {
+            return NULL;
+        }
+        TreeNode *root = new TreeNode(preorder[start]);
+        int leftStart = start + 1;
+        int leftEnd = start;
+        for (int i = leftEnd + 1; i <= end; i++)
+        {
+            if (preorder[i] < preorder[start])
+            {
+                leftEnd++;
+            }
+            else
+            {
+                break;
+            }
+        }
+        root->left = createBST(preorder, leftStart, leftEnd);
+        root->right = createBST(preorder, leftEnd + 1, end);
+        return root;
+    }
+
+    TreeNode *bstFromPreorder(vector<int> &preorder)
+    {
+        return createBST(preorder, 0, preorder.size() - 1);
+    }
+};
